5fa7091de053f5883f74bcb0f59f7dc02b55f66b
Dharmin <dharmin@seatzapp.com>
2023-04-20 22:08:51 -0500

    Basic Login Page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,18 +1,22 @@
-'use client'
+"use client";
 
-import { Auth } from "@supabase/auth-ui-react"
-import { useSupabase } from "../supabase-provider"
-import { ThemeMinimal, ThemeSupa } from "@supabase/auth-ui-shared"
+import { Auth } from "@supabase/auth-ui-react";
+import { useSupabase } from "../supabase-provider";
+import { ThemeMinimal, ThemeSupa } from "@supabase/auth-ui-shared";
 
-export default function LoginPage(){
+export default function LoginPage() {
+  const { supabase } = useSupabase();
 
-
-
-
-
-    return (
-       <>
-Please Login to start using the app
-       </>
-    )
+  return (
+    <>
+      Please Login to start using the app
+      <Auth
+        supabaseClient={supabase}
+        appearance={{ theme: ThemeSupa }}
+        providers={["google"]}
+        onlyThirdPartyProviders
+        theme="dark"
+      />
+    </>
+  );
 }
