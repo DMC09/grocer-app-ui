ff5b7f34b5f3d6cff79be2c994b07b2a1d4d4761
Dharmin <dharmin@seatzapp.com>
2023-04-22 12:28:23 -0500

    basic grocery store component

--- src/app/components/grocerystore.tsx
+++ src/app/components/grocerystore.tsx
@@ -1,24 +1,128 @@
 "use client";
 
-import { Badge, Container, Divider } from "@mui/material";
+import {
+  Badge,
+  Box,
+  CardContent,
+  Collapse,
+  Container,
+  Divider,
+  IconButton,
+  ThemeProvider,
+  Typography,
+} from "@mui/material";
 import TocOutlinedIcon from "@mui/icons-material/TocOutlined";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 import GroceryStoreItem from "./grocerystoreitem";
+import { useEffect, useState } from "react";
+import { GroceryStoreItemType, GroceryStoreWithItemsType } from "@/types";
+import { theme } from "../utils/theme";
+import { useSupabase } from "../supabase-provider";
 
 export default function GroceryStore(data: any) {
-  const groceryStoreItemsToRender = data.grocerystoreitems.map((grocerystoreitem:any)=>{
-    return <GroceryStoreItem key={grocerystoreitem.id} {...grocerystoreitem}/>
-  })
+  const [open, setOpen] = useState(false);
+  const { supabase } = useSupabase();
+  const [groceryStore, setGroceryStore] =
+    useState<GroceryStoreWithItemsType>(data);
+
+
+  //use effect to set up the realtime update.
+  const groceryStoreItemsToRender = groceryStore.grocerystoreitems.map(
+    (grocerystoreitem: GroceryStoreItemType) => {
+      return (
+        <GroceryStoreItem key={grocerystoreitem.id} {...grocerystoreitem} />
+      );
+    }
+  );
+
+  useEffect(() => {
+    const channel = supabase
+      .channel("custom-filter-channel")
+      .on(
+        "postgres_changes",
+        {
+          event: "DELETE",
+          schema: "public",
+          table: "grocerystoreitems",
+          filter: `storeId=eq.${groceryStore.id}`,
+        },
+        (payload) => {
+          console.log("Delete received!", payload);
+        }
+      )
+      .subscribe();
+
+    return () => {
+      supabase.removeChannel(channel);
+    };
+  }, [supabase]);
 
   return (
     <>
-      <Container sx={{ mt: 2 }} maxWidth="sm">
-        <Badge badgeContent={data?.quantity} color="secondary">
-          <TocOutlinedIcon color="error" />
-        </Badge>
-        <h1>{data.name}</h1>
-        {groceryStoreItemsToRender}
-      </Container>
-      <Divider variant="middle" />
+      <ThemeProvider theme={theme}>
+        <Container
+          sx={{
+            p: 1,
+            bgcolor: "background.paper",
+            borderRadius: 1,
+            border: 1,
+            borderColor: "error.main",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              p: 1,
+              m: 6,
+              bgcolor: "background.paper",
+              borderRadius: 1,
+              border: 1,
+              borderColor: "error.main",
+            }}
+          >
+            <Typography variant="h1" color="text.secondary">
+              {data.name}
+            </Typography>
+
+            <IconButton
+              onClick={() => setOpen(!open)}
+              aria-label="expand"
+              size="small"
+            >
+              {open ? (
+                <KeyboardArrowUpIcon />
+              ) : (
+                <Badge badgeContent={data?.quantity} color="secondary">
+                  <TocOutlinedIcon color="error" />
+                </Badge>
+              )}
+            </IconButton>
+          </Box>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <CardContent>
+              <Container
+                sx={{
+                  border: 1,
+                  display: "flex",
+                  justifyContent: "flex-start",
+                  bgcolor: "background.paper",
+                  borderRadius: 10,
+                  borderColor: "primary.main",
+                }}
+                style={{ overflowX: "scroll" }}
+              >
+                {groceryStoreItemsToRender.length > 0 ? (
+                  groceryStoreItemsToRender
+                ) : (
+                  <p>There are no items!</p>
+                )}
+              </Container>
+            </CardContent>
+          </Collapse>
+        </Container>
+      </ThemeProvider>
     </>
   );
 }
