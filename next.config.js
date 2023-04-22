860c7b70059ca2a5c2a1acc927a63147b364d4b9
Dharmin <dharmin@seatzapp.com>
2023-04-22 12:28:32 -0500

    updated next config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '1000logos.net',
+      },
+    ],
+  },
+
 }
 
 module.exports = nextConfig
