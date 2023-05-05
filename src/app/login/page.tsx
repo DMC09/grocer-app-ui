9432dbe65adc35627722856b10ced247a44ebdac
Dharmin <dharmin@seatzapp.com>
2023-05-04 22:17:52 -0500

    updated login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,20 +3,33 @@
 import { Auth } from "@supabase/auth-ui-react";
 import { useSupabase } from "../supabase-provider";
 import { ThemeMinimal, ThemeSupa } from "@supabase/auth-ui-shared";
+import { Box, Container, Typography } from "@mui/material";
 
 export default function LoginPage() {
   const { supabase } = useSupabase();
 
   return (
     <>
-      Please Login to start using the app
-      <Auth
-        supabaseClient={supabase}
-        appearance={{ theme: ThemeSupa }}
-        providers={["google"]}
-        onlyThirdPartyProviders
-        theme="dark"
-      />
+      <Container
+    maxWidth={false}
+      sx={{display: 'flex',flexFlow:"center",justifyContent: 'center',backgroundColor:"primary.light",border:2}}
+      >
+        <Box 
+        
+        >
+
+<Typography variant="body1" color="text.secondary">
+    Please sign in to use 
+            </Typography>
+          <Auth
+
+            supabaseClient={supabase}
+            appearance={{ theme: ThemeSupa }}
+            providers={["google"]}
+            theme="dark"
+          />
+        </Box>
+      </Container>
     </>
   );
 }
