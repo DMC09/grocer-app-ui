66ce4ecf7b6fba16df5f7a579ea8eae08ebbe487
Dharmin <dharmin@seatzapp.com>
2023-05-22 10:20:08 -0500

    moved provider and listener

---
+++ src/app/components/supabase/superbase-listener.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { useSupabase } from "./supabase-provider";
+
+// this component handles refreshing server data when the user logs in or out
+// this method avoids the need to pass a session down to child components
+// in order to re-render when the user's session changes
+// #elegant!
+export default function SupabaseListener({
+  serverAccessToken,
+}: {
+  serverAccessToken?: string;
+}) {
+  const { supabase } = useSupabase();
+  const router = useRouter();
+
+  useEffect(() => {
+    supabase.auth.onAuthStateChange((event, session) => {
+      if (event == "SIGNED_IN") {
+        console.log('Signed in Event in the listener');
+        router.push("/dashboard");
+      }
+    });
+
+    supabase.auth.onAuthStateChange((event, session) => {
+      if (event == "SIGNED_OUT") {
+        console.log('Signed Out Event in the listener');
+        router.push("/login");
+      }
+    });
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((event, session) => {
+
+      if (session?.access_token !== serverAccessToken) {
+        // server and client are out of sync
+        // reload the page to fetch fresh server data
+        // https://beta.nextjs.org/docs/data-fetching/mutating
+        router.refresh();
+      }
+    });
+
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, [serverAccessToken, router, supabase]);
+
+  useEffect(() => {
+    //Listen to signed in event? and then push the ruter
+
+    return () => {
+      //unsuscrbie
+    };
+  }, []);
+
+  return null;
+}
