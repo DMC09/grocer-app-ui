578205c6ad5a82fdacf7f2b0715b0b102d6273b3
Dharmin <dharmin@seatzapp.com>
2023-05-22 10:13:11 -0500

    updated MUI theme

--- src/app/utils/theme.ts
+++ src/app/utils/theme.ts
@@ -4,19 +4,19 @@ import { green, purple } from '@mui/material/colors';
 export const theme = createTheme({
   palette: {
     primary: {
-      light: '#757ce8',
-      main: '#3f50b5',
-      dark: '#002884',
+      light: '#c0c6d6',
+      main: '#071236',
+      dark: '#00041d',
       contrastText: '#fff',
     },
     secondary: {
-      light: '#ff7961',
-      main: '#f44336',
-      dark: '#ba000d',
+      light: '#EAEAEA',
+      main: '#EAEAEA',
+      dark: '#9e9e9e',
       contrastText: '#000',
     },
     background: {
-        paper: '#FFFDD0',
+        paper: '#5F1A37',
         default: '#757ce8'
       },
   },
