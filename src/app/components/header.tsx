aa9c017d9898cae0dec0aa587cae2a4196bbfa2b
Dharmin <dharmin@seatzapp.com>
2023-05-23 23:24:18 -0500

    updated header

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,13 +1,20 @@
 "use client";
 
-import { Box, AppBar, Toolbar, IconButton } from "@mui/material";
-import { Typography, Button } from "@supabase/ui";
+import {
+  Box,
+  AppBar,
+  Toolbar,
+  IconButton,
+  ThemeProvider,
+  Button,
+} from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import { useSupabase } from "./supabase/supabase-provider";
 import { useEffect, useState } from "react";
 import { Auth } from "@supabase/auth-ui-react";
 import HomeIcon from "@mui/icons-material/Home";
 import { useParams, useRouter } from "next/navigation";
+import { theme } from "../utils/theme";
 
 export default function Header() {
   const { supabase, session } = useSupabase();
@@ -37,22 +44,44 @@ export default function Header() {
   }, []);
 
   return (
-    <Box  sx={{ display: "flex", justifyContent: "center",backgroundColor:"primary.main" }}>
-      <AppBar sx={{}} position="static">
-        <Toolbar sx={{ justifyContent: "right" }}>
-          <>
-            <IconButton onClick={() => router.push("/")}>
-              <HomeIcon />
-            </IconButton>
+    <ThemeProvider theme={theme}>
+      <Box
+        sx={{
+        }}
+      >
+        {/* must keep this so it always stays!! */}
+        <AppBar sx={{ backgroundColor: "primary.dark" }} position="static">
+          <Toolbar sx={{ justifyContent: "right" }}>
+            <>
+              <IconButton
+                sx={{
+                  color: "primary.light",
+                }}
+                onClick={() => router.push("/")}
+              >
+                <HomeIcon
+                  sx={{
+                    color: "primary.light",
+                  }}
+                />
+              </IconButton>
 
-            {user && (
-              <Button onClick={handleSignOut} color="inherit">
-                Sign out
-              </Button>
-            )}
-          </>
-        </Toolbar>
-      </AppBar>
-    </Box>
+              {user && (
+                <Button
+                  sx={{
+                    color: "secondary.main",
+                    backgroundColor: "primary.light",
+                  }}
+                  onClick={handleSignOut}
+                  color="inherit"
+                >
+                  Sign out
+                </Button>
+              )}
+            </>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </ThemeProvider>
   );
 }
