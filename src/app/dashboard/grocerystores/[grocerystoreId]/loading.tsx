6bc8ccac6565d5273459c3f02c763b053bd99dbb
Dharmin <dharmin@seatzapp.com>
2023-05-23 23:20:36 -0500

    loading for a grocery store

--- src/app/dashboard/grocerystores/[grocerystoreId]/loading.tsx
+++ src/app/dashboard/grocerystores/[grocerystoreId]/loading.tsx
@@ -1,8 +1,14 @@
-"use client"
+"use client";
 
 import Skeleton from "@mui/material/Skeleton";
 
 export default function Loading() {
   // You can add any UI inside Loading, including a Skeleton.
-  return <Skeleton sx={{color:"green"}} variant="rectangular" width={210} height={118} />;
+  return (
+    <>
+      <Skeleton variant="rounded" width={350} height={400} />
+      <Skeleton variant="rounded" width={350} height={400} />
+      <Skeleton variant="rounded" width={350} height={400} />
+    </>
+  );
 }
