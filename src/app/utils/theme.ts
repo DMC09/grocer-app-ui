3339925fea63bdf694c7d304568a84fe6d77032a
Dharmin <dharmin@seatzapp.com>
2023-05-23 23:18:44 -0500

    updated theme and types

--- src/app/utils/theme.ts
+++ src/app/utils/theme.ts
@@ -1,23 +1,25 @@
-import { createTheme } from '@mui/material/styles';
-import { green, purple } from '@mui/material/colors';
+import { createTheme } from "@mui/material/styles";
 
 export const theme = createTheme({
+  typography: {
+
+  },
   palette: {
     primary: {
-      light: '#c0c6d6',
-      main: '#071236',
-      dark: '#00041d',
-      contrastText: '#fff',
+      light: "#c0c6d6",
+      main: "#071236",
+      dark: "#00041d",
+      contrastText: "#fff",
     },
     secondary: {
-      light: '#EAEAEA',
-      main: '#EAEAEA',
-      dark: '#9e9e9e',
-      contrastText: '#000',
+      light: "#EAEAEA",
+      main: "#EAEAEA",
+      dark: "#9e9e9e",
+      contrastText: "#000",
     },
     background: {
-        paper: '#5F1A37',
-        default: '#757ce8'
-      },
+      paper: "#e5e3d9",
+      default: "#FFFFFF",
+    },
   },
 });
