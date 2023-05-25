633508204594f9e184c0ace08ae2f22e8a0f1ca9
Dharmin <dharmin@seatzapp.com>
2023-05-24 22:06:23 -0500

    added component for editing and deleting store

---
+++ src/app/components/groceryStore/groceryStoreHeaderMenu.tsx
@@ -0,0 +1,122 @@
+"use client";
+
+import {
+  Avatar,
+  Divider,
+  IconButton,
+  ListItemIcon,
+  Menu,
+  MenuItem,
+} from "@mui/material";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import { useState } from "react";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { Settings, Logout } from "@mui/icons-material";
+import { useSupabase } from "../supabase/supabase-provider";
+import { useRouter } from "next/navigation";
+
+export default function GroceryStoreHeaderMenu({
+  storeId,
+}: {
+  storeId: number;
+}) {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const { supabase, session } = useSupabase();
+  const router = useRouter();
+
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+
+  const handleDelete = async () => {
+    const { data, error } = await supabase
+      .from("grocerystores")
+      .delete()
+      .eq("id", storeId)
+      .select();
+
+    if (data && data.length > 0) {
+      router.push("/dashboard");
+    }
+    if (error) {
+      throw new Error(error.message);
+    }
+  };
+
+  return (
+    <>
+      <IconButton
+        sx={{ color: "background.paper" }}
+        aria-label="more"
+        id="long-button"
+        aria-controls={open ? "long-menu" : undefined}
+        aria-expanded={open ? "true" : undefined}
+        aria-haspopup="true"
+        onClick={handleClick}
+      >
+        <MoreVertIcon />
+      </IconButton>
+      <Menu
+        anchorEl={anchorEl}
+        id="account-menu"
+        open={open}
+        onClose={handleClose}
+        onClick={handleClose}
+        PaperProps={{
+          elevation: 0,
+          sx: {
+            overflow: "visible",
+            filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+            mt: 1.5,
+            "& .MuiAvatar-root": {
+              width: 32,
+              height: 32,
+              ml: -0.5,
+              mr: 1,
+            },
+            "&:before": {
+              content: '""',
+              display: "block",
+              position: "absolute",
+              top: 0,
+              right: 14,
+              width: 10,
+              height: 10,
+              bgcolor: "background.paper",
+              transform: "translateY(-50%) rotate(45deg)",
+              zIndex: 0,
+            },
+          },
+        }}
+        transformOrigin={{ horizontal: "right", vertical: "top" }}
+        anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+      >
+        {/* <MenuItem onClick={handleClose}>
+          <Avatar /> Profile
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <Avatar /> My account
+        </MenuItem> */}
+        <Divider />
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <Settings fontSize="small" />
+          </ListItemIcon>
+          Store Settings
+        </MenuItem>
+        <MenuItem onClick={handleDelete}>
+          <ListItemIcon>
+            <DeleteForeverIcon fontSize="small" />
+          </ListItemIcon>
+          Delete Store
+          {/* need to add the signout finctionliaty to the onClick handlers  */}
+        </MenuItem>
+      </Menu>
+    </>
+  );
+}
