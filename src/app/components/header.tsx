32f677646fadf9476a6e03a88b38c8e5060eeb48
Dharmin <dharmin@seatzapp.com>
2023-05-24 22:05:30 -0500

    updated to conditionally render based on user

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -7,6 +7,11 @@ import {
   IconButton,
   ThemeProvider,
   Button,
+  Avatar,
+  Divider,
+  ListItemIcon,
+  Menu,
+  MenuItem,
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import { useSupabase } from "./supabase/supabase-provider";
@@ -15,69 +20,35 @@ import { Auth } from "@supabase/auth-ui-react";
 import HomeIcon from "@mui/icons-material/Home";
 import { useParams, useRouter } from "next/navigation";
 import { theme } from "../utils/theme";
+import { PersonAdd, Settings, Logout } from "@mui/icons-material";
+import HeaderMenu from "./headerMenu";
 
 export default function Header() {
   const { supabase, session } = useSupabase();
   const router = useRouter();
 
-  const [user, setUser] = useState<unknown>(null);
-  // const [session, setSession] = useState<unknown>(null);
-
-  const getSession = async () => {
-    const { data, error } = await supabase.auth.getSession();
-    if (data) {
-      // setSession(data?.session);
-      setUser(data?.session?.user);
-    } else {
-      console.log(error);
-    }
-  };
-
-  const handleSignOut = async () => {
-    await supabase.auth.signOut();
-    // await setSession(null);
-    await setUser(null);
-  };
-
-  useEffect(() => {
-    getSession();
-  }, []);
-
   return (
     <ThemeProvider theme={theme}>
-      <Box
-        sx={{
-        }}
-      >
+      <Box sx={{}}>
         {/* must keep this so it always stays!! */}
         <AppBar sx={{ backgroundColor: "primary.dark" }} position="static">
-          <Toolbar sx={{ justifyContent: "right" }}>
+          <Toolbar sx={{ justifyContent: "space-between" }}>
             <>
-              <IconButton
-                sx={{
-                  color: "primary.light",
-                }}
-                onClick={() => router.push("/")}
-              >
-                <HomeIcon
+              {session?.user && (
+                <IconButton
                   sx={{
                     color: "primary.light",
                   }}
-                />
-              </IconButton>
-
-              {user && (
-                <Button
-                  sx={{
-                    color: "secondary.main",
-                    backgroundColor: "primary.light",
-                  }}
-                  onClick={handleSignOut}
-                  color="inherit"
+                  onClick={() => router.push("/")}
                 >
-                  Sign out
-                </Button>
+                  <HomeIcon
+                    sx={{
+                      color: "primary.light",
+                    }}
+                  />
+                </IconButton>
               )}
+              {session?.user && <HeaderMenu />}
             </>
           </Toolbar>
         </AppBar>
