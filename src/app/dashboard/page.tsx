5cbea140476f45413a226a05c7ee8a3ab99f1e16
Dharmin <dharmin@seatzapp.com>
2023-05-29 14:47:19 -0500

    removed unecessary logic

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -19,12 +19,13 @@ import {
   TextField,
 } from "@mui/material";
 import { useSupabase } from "../components/supabase/supabase-provider";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import ClickAwayListener from "@mui/base/ClickAwayListener";
 import { GroceryStoreWithItemsType, GroceryStoreType } from "@/types";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
 import { useRouter } from "next/navigation";
 import DashboardHeader from "../components/dashboardHeader";
+import { PostgrestError } from "@supabase/supabase-js";
 
 export default function Dashboard() {
   const router = useRouter();
@@ -38,10 +39,14 @@ export default function Dashboard() {
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
   // TODO:move the add new store to it's own component
-  const [newGroceryStoreName, setNewGroceryStoreName] = useState<string>("");
+  // TODo: implment backgrou MUI when doing stuff?
 
   const getAllGroceryStores = async () => {
-    const { data, error } = await supabase.from("grocerystores").select("*");
+    const {
+      data,
+      error,
+    }: { data: GroceryStoreType[] | [] | null; error: PostgrestError | null } =
+      await supabase.from("grocerystores").select("*");
     if (data) {
       setIsLoading(false);
       setGroceryStores(data);
@@ -80,127 +85,65 @@ export default function Dashboard() {
   }, [supabase]);
 
   // figure out a order for the stores
-  const groceryStoresToRender = groceryStores?.map((groceryStore: GroceryStoreType) => {
-    return (
-      <>
-        <Badge color="secondary" badgeContent={groceryStore.quantity}>
-          <Card
-            raised
-            sx={{
-              border: 1,
-              borderColor: "primary",
-              background: "primary",
-              borderRadius: 1,
-              width: 350,
-              height: 300,
-            }}
-            style={{ flexShrink: 0 }}
-          >
-            <CardActionArea
-              onClick={() => {
-                router.push(`/dashboard/grocerystores/${groceryStore.id}`);
+  const groceryStoresToRender = groceryStores?.map(
+    (groceryStore: GroceryStoreType) => {
+      return (
+        <>
+          <Badge color="secondary" badgeContent={groceryStore.quantity}>
+            <Card
+              raised
+              sx={{
+                border: 1,
+                borderColor: "primary",
+                background: "primary",
+                borderRadius: 1,
+                width: 350,
+                height: 300,
               }}
+              style={{ flexShrink: 0 }}
             >
-              <CardHeader title={groceryStore.name} />
-              <CardMedia
-                component="img"
-                height={250}
-                image={groceryStore?.image || ""}
-                alt={`Image of${groceryStore.name} `}
-                sx={{ objectFit: "fill" }}
-              />
-              {/* <CardContent></CardContent> */}
-            </CardActionArea>
-            {/* <CardActions></CardActions> */}
-          </Card>
-        </Badge>
-      </>
-    );
-  });
-
-  async function handleClickOpen() {
-    setOpen(true);
-  }
-
-  async function handleSubmit() {
-    const { data, error } = await supabase
-      .from("grocerystores")
-      .insert([{ name: newGroceryStoreName }]);
-    if (error) {
-      throw new Error(error.message);
-    } else {
-      setOpen(false);
-      setNewGroceryStoreName("");
+              <CardActionArea
+                onClick={() => {
+                  router.push(`/dashboard/grocerystores/${groceryStore?.id}`);
+                }}
+              >
+                <CardHeader title={groceryStore.name} />
+                <CardMedia
+                  component="img"
+                  height={250}
+                  image={groceryStore?.image || ""}
+                  alt={`Image of${groceryStore.name} `}
+                  sx={{ objectFit: "fill" }}
+                />
+                {/* <CardContent></CardContent> */}
+              </CardActionArea>
+              {/* <CardActions></CardActions> */}
+            </Card>
+          </Badge>
+        </>
+      );
     }
-  }
-
-  function handleClose(event: {}): void {
-    setOpen(false);
-    setNewGroceryStoreName("");
-  }
-
-  function handleClickAway(event: MouseEvent | TouchEvent): void {
-    console.log("Clciked away");
-  }
+  );
 
   return (
     <>
-      {/* move the contianer as part of the layout that way there is continuity */}
-      {/* The loading will container just the skelton */}
-      {/* Make this container take up the full width in the layout */}
-      {/* Make the box take move the space and have it the inverse of the colors itself. */}
-
-      <div>
-        <ClickAwayListener onClickAway={handleClickAway}>
-          <Dialog open={open} onClose={handleClose}>
-            <DialogTitle>Add new Store</DialogTitle>
-            <DialogContent>
-              <TextField
-                autoFocus
-                margin="dense"
-                id="Name"
-                label="Name"
-                type="email"
-                fullWidth
-                variant="standard"
-                onChange={(e) => setNewGroceryStoreName(e.target.value)}
-                value={newGroceryStoreName}
-              />
-            </DialogContent>
-            <DialogActions>
-              <Button onClick={handleClose}>Cancel</Button>
-              <Button onClick={handleSubmit}>Submit</Button>
-            </DialogActions>
-          </Dialog>
-        </ClickAwayListener>
-      </div>
-      <>
       <DashboardHeader />
-        <Container
+      <Container
         maxWidth={false}
-          sx={{
-            display: "flex",
-            flexFlow: "row",
-            flexWrap: "wrap",
-            justifyContent: "center",
-            backgroundColor:"primary.light",
-            gap: 3,
-            py:2,
-            border:2
-          }}
-        >
-          {/* this needs it's own container */}
-          {groceryStores && groceryStores.length > 0 && groceryStoresToRender}
-        </Container>
-
-        {/* {isLoading ? (
-            <p>loading</p>
-          ) : groceryStores?.length > 0 ? (
-            groceryStoresToRender
-          ) : (
-            <p>nothing to see!</p>
-          )} */}
-      </>
+        sx={{
+          display: "flex",
+          flexFlow: "row",
+          flexWrap: "wrap",
+          justifyContent: "center",
+          backgroundColor: "primary.light",
+          gap: 3,
+          py: 2,
+          border: 2,
+        }}
+      >
+        {/* this needs it's own container */}
+        {groceryStores && groceryStores.length > 0 && groceryStoresToRender}
+      </Container>
     </>
   );
 }
