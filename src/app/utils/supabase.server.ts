c7c8241107f77e5e768f45547c9184e48a33aec0
Dharmin <dharmin@seatzapp.com>
2023-05-31 16:36:59 -0500

    updated import

--- src/app/utils/supabase.server.ts
+++ src/app/utils/supabase.server.ts
@@ -1,6 +1,7 @@
 import { headers, cookies } from 'next/headers';
 import { createServerComponentSupabaseClient } from '@supabase/auth-helpers-nextjs';
-import { Database } from '@/lib/supabase';
+import { Database } from '@/types';
+
 
 
 export const createServerClient = () =>
