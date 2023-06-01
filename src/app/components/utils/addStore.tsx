7543c0bf496b6784597249cbab158ada22f64f5c
Dharmin <dharmin@seatzapp.com>
2023-05-31 23:14:33 -0500

    added rough image uploading function

--- src/app/components/utils/addStore.tsx
+++ src/app/components/utils/addStore.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Dialog,
   DialogTitle,
@@ -5,11 +7,15 @@ import {
   TextField,
   DialogActions,
   Button,
+  Card,
+  CardMedia,
 } from "@mui/material";
 import { ChangeEvent, useMemo, useState } from "react";
 import { useSupabase } from "../supabase/supabase-provider";
 import { PostgrestError } from "@supabase/supabase-js";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
+import AddPhotoAlternateIcon from "@mui/icons-material/AddPhotoAlternate";
+import image from "next/image";
 
 export default function AddStore() {
   const { supabase, session } = useSupabase();
@@ -17,6 +23,7 @@ export default function AddStore() {
   const [open, setOpen] = useState<boolean>(false);
   const [isInvalid, setIsInvalid] = useState<boolean | null>(null);
   const [errorText, setErrorText] = useState<string | null>(null);
+  const [image, setImage] = useState({ preview: "", raw: "" });
   const [newGroceryStoreName, setNewGroceryStoreName] = useState<string>("");
 
   const getSelectId = useMemo(async (): Promise<number | null> => {
@@ -68,15 +75,45 @@ export default function AddStore() {
     setIsInvalid(null);
   }
 
+  const handleImageSet = async (event: any) => {
+    if (event.target.files.length) {
+      setImage({
+        preview: URL.createObjectURL(event.target.files[0]),
+        raw: event.target.files[0],
+      });
+    }
+  };
+  const handleImageUpload = async () => {
+    if (image.raw) {
+      const { data, error } = await supabase.storage
+        .from("grocerystore")
+        .upload("grocerystore_images/test.png", image.raw);
+      console.log(data, "data after uploading");
+      if (error) {
+        throw new Error(`Error uploading image ${error.message}`);
+      } else {
+        const { data } = supabase.storage
+          .from("grocerystore")
+          .getPublicUrl("grocerystore_images/test.png");
+
+        if (data) {
+          return data.publicUrl;
+        } else {
+          throw new Error("error retrieving public url image");
+        }
+      }
+    }
+  };
+
   async function handleSubmit() {
     const isValidResult = await validation();
+    const public_url = await handleImageUpload();
 
     if (isValidResult) {
-      console.log("form submiting??");
       const select_id = await getSelectId;
       const { data, error } = await supabase
         .from("grocerystores")
-        .insert([{ name: newGroceryStoreName, select_id }]);
+        .insert([{ name: newGroceryStoreName, select_id, image: public_url }]);
       if (error) {
         throw new Error(error.message);
       } else {
@@ -112,6 +149,48 @@ export default function AddStore() {
             onChange={handleChange}
             value={newGroceryStoreName}
           />
+        </DialogContent>
+        <DialogContent>
+          <>
+            {image.preview ? (
+              <Card
+                sx={{
+                  maxWidth: 150,
+                }}
+              >
+                <CardMedia
+                  component="img"
+                  height="150"
+                  image={image.preview}
+                  alt={`Image of `}
+                />
+              </Card>
+            ) : (
+              <Card
+                sx={{
+                  maxWidth: 150,
+                }}
+              >
+                <CardMedia
+                  component="img"
+                  height="150"
+                  image={
+                    "https://filetandvine.com/wp-content/uploads/2015/07/pix-uploaded-placeholder.jpg"
+                  }
+                  alt={`Image of `}
+                />
+              </Card>
+            )}
+
+            <Button
+              variant="contained"
+              component="label"
+              startIcon={<AddPhotoAlternateIcon />}
+            >
+              Upload File
+              <input type="file" onChange={handleImageSet} hidden />
+            </Button>
+          </>
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose}>Cancel</Button>
