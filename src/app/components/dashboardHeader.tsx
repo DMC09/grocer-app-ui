b246c24895f9a5abfdab5226fe549f56a14ef8cf
Dharmin <dharmin@seatzapp.com>
2023-06-01 21:24:52 -0500

    removed code

--- src/app/components/dashboardHeader.tsx
+++ src/app/components/dashboardHeader.tsx
@@ -1,25 +1,9 @@
 "use client";
 
-import {
-  Box,
-  AppBar,
-  Toolbar,
-  IconButton,
-  ThemeProvider,
-  Button,
-  Typography,
-  Card,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogTitle,
-  TextField,
-} from "@mui/material";
+import { ThemeProvider, Typography, Card } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import { useSupabase } from "./supabase/supabase-provider";
-import { useEffect, useMemo, useState } from "react";
-import { Auth } from "@supabase/auth-ui-react";
-import HomeIcon from "@mui/icons-material/Home";
+import { useCallback, useEffect, useMemo, useState } from "react";
 
 import { theme } from "../utils/theme";
 import { PostgrestError } from "@supabase/supabase-js";
@@ -29,62 +13,23 @@ export default function DashboardHeader() {
   //TODO: move the add store logic to it's own file.
   const { supabase, session } = useSupabase();
 
-  const [open, setOpen] = useState<boolean>(false);
-  const [isValid, setIsValid] = useState<boolean>(false);
-  const [newGroceryStoreName, setNewGroceryStoreName] = useState<string>("");
+  const [selectId, setSelectId] = useState<string | null>(null);
 
-  const getSelectId = useMemo(async (): Promise<number | null> => {
-    const { data, error }: { data: any; error: PostgrestError | null } =
-      await supabase.from("profiles").select("select_id").single();
-    if (data) {
-      return data?.select_id;
-    } else {
-      throw new Error(error?.message);
-      return null;
+  useEffect(() => {
+    async function getSelectId() {
+      const { data, error } = await supabase
+        .from("profiles")
+        .select("select_id")
+        .single();
+      if (error) {
+        throw new Error(error.message);
+      } else {
+        setSelectId(data?.select_id);
+      }
     }
+
+    getSelectId();
   }, []);
-
-  async function handleClickOpen() {
-    setOpen(true);
-  }
-
-  function handleChange(text: string): void {
-    setNewGroceryStoreName(text);
-  }
-
-  function handleClose(event: {}): void {
-    setOpen(false);
-    setNewGroceryStoreName("");
-  }
-
-  function isValidInput(text: string) {
-    // Check if the text is empty
-    if (text.trim() === "") {
-      return "The text is empty.";
-    }
-
-    // Check if the text is valid alphanumeric
-    const regExp = /^[a-zA-Z0-9]+$/;
-    if (!regExp.test(text)) {
-      return "The text is not valid alphanumeric.";
-    } else {
-      return;
-    }
-  }
-
-  async function handleSubmit() {
-    // logic to stop submission if ther is not correct submisiso n
-    const select_id = await getSelectId;
-    const { data, error } = await supabase
-      .from("grocerystores")
-      .insert([{ name: newGroceryStoreName, select_id }]);
-    if (error) {
-      throw new Error(error.message);
-    } else {
-      setOpen(false);
-      setNewGroceryStoreName("");
-    }
-  }
 
   return (
     <>
@@ -100,9 +45,9 @@ export default function DashboardHeader() {
         >
           <div></div>
           <Typography align="center" color="#EAEAEA" variant="h3">
-            Dashboard!!
+            Dashboard
           </Typography>
-          <AddStore />
+          <AddStore select_id={selectId} />
         </Card>
       </ThemeProvider>
     </>
