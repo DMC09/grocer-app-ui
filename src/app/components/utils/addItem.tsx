b401a44ad5f55dff563db6f73729176310f4a654
Dharmin <dharmin@seatzapp.com>
2023-06-03 23:15:04 -0500

    updated to inlcude uploading image

--- src/app/components/utils/addItem.tsx
+++ src/app/components/utils/addItem.tsx
@@ -9,12 +9,15 @@ import {
   Box,
   Container,
   IconButton,
+  Card,
+  CardMedia,
 } from "@mui/material";
+import AddPhotoAlternateIcon from "@mui/icons-material/AddPhotoAlternate";
 import ControlPointIcon from "@mui/icons-material/ControlPoint";
-
 import { useState } from "react";
 import { useSupabase } from "../supabase/supabase-provider";
 
+// Create upload image capability
 export default function AddItem({
   store_id,
   select_id,
@@ -29,14 +32,55 @@ export default function AddItem({
   const [quantity, setQuantity] = useState("");
   const [name, setName] = useState("");
   const [notes, setNotes] = useState("");
+  const [image, setImage] = useState({ preview: "", raw: "" });
+  const [imagePath, setImagePath] = useState<string | null>(null);
 
   const handleClickOpen = () => {
     setOpen(true);
   };
 
+  async function generateImagePath(select_id: string) {
+    //Formula is last 16 characters of select_id + Current DateTime in seconds/
+    const lastPartOfSelectId = select_id?.slice(-16);
+    const currentTimeStamp = new Date().getTime();
+
+    setImagePath(
+      `grocerystoreitem_images/${lastPartOfSelectId}/${currentTimeStamp}`
+    );
+  }
+  async function handleImageSet(event: any) {
+    if (event.target.files.length && select_id) {
+      generateImagePath(select_id);
+      setImage({
+        preview: URL.createObjectURL(event.target.files[0]),
+        raw: event.target.files[0],
+      });
+    }
+  }
+
+  async function handleImageUpload() {
+    console.log(image.raw, "path for image when uploading");
+    console.log(imagePath, "path for image when uploading");
+    if (image.raw && imagePath) {
+      const { data, error } = await supabase.storage
+        .from("grocerystore")
+        // Need a custom path thing for this.
+        // Also need to getthe public url
+        .upload(imagePath, image.raw);
+      if (error) {
+        throw new Error(`Error uploading image ${error.message}`);
+      } else {
+        console.log(data, "image uploaded successfully");
+      }
+    }
+  }
+
   // function handleClickAway(event: MouseEvent | TouchEvent): void {
   //   throw new Error("Function not implemented.");
   // }
+  //TODO: Do the same type of validation for the items in the store
+  //Also make sure to fix all tables so they cann't take null for certain things.
+  // prevent evet putting zero
 
   const handleClose = () => {
     setOpen(false);
@@ -46,26 +90,51 @@ export default function AddItem({
   };
 
   async function handleSubmit() {
-    const { data, error } = await supabase
-      .from("grocerystoreitems")
-      .insert([
-        {
-          store_id,
-          name,
-          notes,
-          quantity: Number(quantity),
-          select_id,
-        },
-      ])
-      .select();
-
-    if (data) {
-      setOpen(false);
-      setName("");
-      setNotes("");
-      setQuantity("");
-    } else if (error) {
-      throw new Error(error.message);
+    if (image.raw) {
+      await handleImageUpload();
+      const { data, error } = await supabase
+        .from("grocerystoreitems")
+        .insert([
+          {
+            store_id,
+            name,
+            notes,
+            quantity: Number(quantity),
+            select_id,
+            image: imagePath,
+          },
+        ])
+        .select();
+      if (data) {
+        setOpen(false);
+        setName("");
+        setNotes("");
+        setQuantity("");
+        setImage({ preview: "", raw: "" });
+      } else if (error) {
+        throw new Error(error.message);
+      }
+    } else {
+      const { data, error } = await supabase
+        .from("grocerystoreitems")
+        .insert([
+          {
+            store_id,
+            name,
+            notes,
+            quantity: Number(quantity),
+            select_id,
+          },
+        ])
+        .select();
+      if (data) {
+        setOpen(false);
+        setName("");
+        setNotes("");
+        setQuantity("");
+      } else if (error) {
+        throw new Error(error.message);
+      }
     }
   }
 
@@ -118,6 +187,52 @@ export default function AddItem({
             onChange={(e) => setQuantity(e.target.value)}
             value={quantity}
           />
+        </DialogContent>
+        <DialogContent
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            flexFlow: "column",
+          }}
+        >
+          {image.preview ? (
+            <Card
+              sx={{
+                maxWidth: 150,
+              }}
+            >
+              <CardMedia
+                component="img"
+                height="150"
+                image={image.preview}
+                alt={`Image of `}
+              />
+            </Card>
+          ) : (
+            <Card
+              sx={{
+                maxWidth: 150,
+              }}
+            >
+              <CardMedia
+                component="img"
+                height="150"
+                image={
+                  "https://filetandvine.com/wp-content/uploads/2015/07/pix-uploaded-placeholder.jpg"
+                }
+                alt={`Image of `}
+              />
+            </Card>
+          )}
+          <Button
+            variant="contained"
+            component="label"
+            startIcon={<AddPhotoAlternateIcon />}
+          >
+            Upload File
+            <input type="file" onChange={handleImageSet} hidden />
+          </Button>
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose}>Cancel</Button>
