c22e8d12d99e7c64a12429201d7e96ffd31ffad3
Dharmin <dharmin@seatzapp.com>
2023-06-08 19:51:51 -0500

    added join group

---
+++ src/app/components/utils/joinGroup.tsx
@@ -0,0 +1,94 @@
+import GroupAddIcon from "@mui/icons-material/GroupAdd";
+import {
+  Button,
+  Card,
+  CardMedia,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  IconButton,
+  TextField,
+} from "@mui/material";
+import { useSupabase } from "../supabase/supabase-provider";
+import { useState } from "react";
+import { ProfileType } from "@/types";
+import AddPhotoAlternateIcon from "@mui/icons-material/AddPhotoAlternate";
+
+export default function JoinGroup(profile: ProfileType | null) {
+  const { supabase, session } = useSupabase();
+  const [open, setOpen] = useState(false);
+  const [shareCode, setShareCode] = useState("");
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    setShareCode("");
+  };
+
+  async function handleSubmit() {
+    // call the RPC
+    // dialog to open a box and enter the code
+    // app thinks and maybe a loading screen
+    // state if successfull, I should refresh the group settings stuff
+    // Failture notify user. but shouldn't really happen
+
+    const { data, error } = await supabase.rpc("join_group_via_share_code", {
+      share_code: shareCode,
+      email: profile?.email,
+      first_name: profile?.first_name,
+      last_name: profile?.last_name,
+      profile_id: profile?.id,
+      profile_image: profile?.avatar_url,
+    });
+
+    if (error) {
+      throw new Error(error.message);
+    }
+
+    if (data) {
+      console.log("Insert successful");
+      return true;
+    } else {
+      console.log("Insert failed");
+      return false;
+    }
+  }
+
+  return (
+    <>
+      <IconButton
+        sx={{ color: "primary.main" }}
+        aria-label="add to grocery store"
+        onClick={handleClickOpen}
+      >
+        Join Group
+        <GroupAddIcon sx={{ fontSize: 25 }} />
+      </IconButton>
+
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Submit Share code</DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="share Code"
+            label="Share Code"
+            type="text"
+            fullWidth
+            variant="standard"
+            onChange={(e) => setShareCode(e.target.value)}
+            value={shareCode}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleSubmit}>Submit</Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+}
