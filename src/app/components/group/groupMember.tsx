5d336d9df64a5f51bf6ab38da0e784eb1a44c5c2
Dharmin <dharmin@seatzapp.com>
2023-06-08 19:56:45 -0500

    group member

---
+++ src/app/components/group/groupMember.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { Typography } from "@mui/material";
+
+export default function GroupMember() {
+  return (
+    <>
+      <Typography color="#071236" variant="body2">
+        group member!
+      </Typography>
+    </>
+  );
+}
