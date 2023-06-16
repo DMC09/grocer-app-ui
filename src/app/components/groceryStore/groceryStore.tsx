82e275441951c5100f8a458b839a30e29f111371
Dharmin <dharmin@seatzapp.com>
2023-06-15 22:40:04 -0500

    conditionally render the views

--- src/app/components/groceryStore/groceryStore.tsx
+++ src/app/components/groceryStore/groceryStore.tsx
@@ -1,4 +1,4 @@
-import { GroceryStoreType } from "@/types";
+import { GroceryStoreProps, GroceryStoreType } from "@/types";
 import {
   Badge,
   Card,
@@ -8,44 +8,72 @@ import {
 } from "@mui/material";
 import { useRouter } from "next/navigation";
 import { useSupabase } from "../supabase/supabase-provider";
+import { useState } from "react";
 
-export default function GroceryStore(groceryStore: GroceryStoreType) {
+
+
+
+export default function GroceryStore({ groceryStore, expanded }: GroceryStoreProps) {
   const router = useRouter();
-  const { supabase } = useSupabase();
+  const { supabase, session } = useSupabase();
 
   return (
     <>
-      <Badge color="secondary" badgeContent={groceryStore.quantity}>
-        <Card
-          key={groceryStore.id}
-          raised
-          sx={{
-            border: 1,
-            borderColor: "primary",
-            background: "primary",
-            borderRadius: 1,
-            maxHeight: 300,
-            width: 300,
-          }}
-          style={{ flexShrink: 0 }}
-        >
-          <CardActionArea
-            onClick={() => {
-              router.push(`/dashboard/grocerystores/${groceryStore?.id}`);
+      { expanded ? (
+        <Badge color="secondary" badgeContent={groceryStore.quantity}>
+          <Card
+            key={groceryStore.id}
+            raised
+            sx={{
+              border: 1,
+              borderColor: "primary",
+              background: "primary",
+              borderRadius: 1,
+              maxHeight: 300,
+              width: 300,
             }}
+            style={{ flexShrink: 0 }}
           >
-            <CardHeader title={groceryStore.name} />
-            <CardMedia
-              height={250}
-              width={300}
-              component="img"
-              image={`${process?.env?.NEXT_PUBLIC_SUPABASE_GROCERYSTORE}/${groceryStore?.image}`}
-              alt={`Image of${groceryStore.name} `}
-              sx={{ objectFit: "fill" }}
-            />
-          </CardActionArea>
-        </Card>
-      </Badge>
+            <CardActionArea
+              onClick={() => {
+                router.push(`/dashboard/grocerystores/${groceryStore?.id}`);
+              }}
+            >
+              <CardHeader title={groceryStore.name} />
+              <CardMedia
+                height={250}
+                width={300}
+                component="img"
+                image={`${process?.env?.NEXT_PUBLIC_SUPABASE_GROCERYSTORE}/${groceryStore?.image}`}
+                alt={`Image of${groceryStore.name} `}
+                sx={{ objectFit: "fill" }}
+              />
+            </CardActionArea>
+          </Card>
+        </Badge>
+      ):  <Badge color="secondary" badgeContent={groceryStore.quantity}>
+      <Card
+        key={groceryStore.id}
+        raised
+        sx={{
+          border: 1,
+          borderColor: "primary",
+          background: "primary",
+          borderRadius: 1,
+          height: "fit-content",
+          width: 200,
+        }}
+        style={{ flexShrink: 0 }}
+      >
+        <CardActionArea
+          onClick={() => {
+            router.push(`/dashboard/grocerystores/${groceryStore?.id}`);
+          }}
+        >
+          <CardHeader style={{ textAlign: 'center' }} title={groceryStore.name} />
+        </CardActionArea>
+      </Card>
+    </Badge>}
     </>
   );
 }
