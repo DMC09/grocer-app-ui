24540fc706739141abd0c4eb562128ad3945d10c
Dharmin <dharmin@seatzapp.com>
2023-06-15 22:40:24 -0500

    getting  profile data to check views to pass in

--- src/app/components/dashboardHeader.tsx
+++ src/app/components/dashboardHeader.tsx
@@ -24,22 +24,23 @@ import { useCallback, useEffect, useMemo, useState } from "react";
 import GridViewIcon from "@mui/icons-material/GridView";
 
 import { theme } from "../utils/theme";
-import { PostgrestError } from "@supabase/supabase-js";
+import { PostgrestError, User } from "@supabase/supabase-js";
 import AddStore from "./utils/addStore";
 import { Settings } from "@mui/icons-material";
 import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import AddPhotoAlternateIcon from "@mui/icons-material/AddPhotoAlternate";
 import image from "next/image";
+import { ProfileType } from "@/types";
 
 export default function DashboardHeader() {
-  //TODO: move the add store logic to it's own file.
   const { supabase, session } = useSupabase();
   const [selectId, setSelectId] = useState<string | null>(null);
+  const [user, SetUser] = useState<User | null | undefined>(session?.user);
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const [profile, SetProfile] = useState<ProfileType | null>(null);
   const open = Boolean(anchorEl);
   const [openDialog, setOpenDialog] = useState<boolean>(false);
-  const [altView, setAltView] = useState<boolean>(false);
 
   const handleClick = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
@@ -51,30 +52,52 @@ export default function DashboardHeader() {
     setOpenDialog(false);
   };
 
-  useEffect(() => {
-    async function getSelectId() {
-      const { data, error } = await supabase
-        .from("profiles")
-        .select("select_id")
-        .eq("id", session?.user.id)
-        .single();
-      if (error) {
-        throw new Error(error.message);
-      } else {
-        setSelectId(data?.select_id);
-      }
+  //maybe get all the profile data an memoiz just the select_id
+  //test if this changes from group
+  async function getProfileData() {
+    const { data, error } = await supabase
+      .from("profiles")
+      .select("*")
+      .eq("id", user?.id)
+      .single();
+    if (error) {
+      throw new Error(error.message);
+    } else {
+      SetProfile(data);
     }
+  }
+  async function getSelectId() {
+    const { data, error } = await supabase
+      .from("profiles")
+      .select("select_id")
+      .eq("id", user?.id)
+      .single();
+    if (error) {
+      throw new Error(error.message);
+    } else {
+      setSelectId(data?.select_id);
+    }
+  }
+  useEffect(() => {
     if (session?.user) {
       getSelectId();
+      getProfileData();
     }
   }, [supabase]);
+  
 
-  function handleDelete() {
-    throw new Error("Function not implemented.");
-  }
+  async function handleChangeView() {
+    const { data, error } = await supabase
+      .from("profiles")
+      .update({ expanded_dashboard: !profile?.expanded_dashboard })
+      .eq("id", profile?.id)
+      .single();
 
-  function handleChangeView() {
-    throw new Error("Function not implemented.");
+    if (error) {
+      throw new Error(error.message);
+    } else {
+      await getProfileData();
+    }
   }
 
   return (
@@ -155,6 +178,7 @@ export default function DashboardHeader() {
                   </ListItemIcon>
                   Change View
                 </MenuItem>
+                <p>{profile?.expanded_dashboard ? "expanded" : "normal"}</p>
               </Menu>
             </Box>
           </Card>
