090119326e832e3a5d247fd0d97e9a27e9822c5f
Dharmin <dharmin@seatzapp.com>
2023-06-17 22:26:18 -0500

    added route protection

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -1,14 +1,20 @@
+import { Database } from "@/types";
+import { createMiddlewareSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { NextResponse } from "next/server";
 
-import { Database } from '@/types'
-import { createMiddlewareSupabaseClient } from '@supabase/auth-helpers-nextjs'
-import { NextResponse } from 'next/server'
-
-import type { NextRequest } from 'next/server'
-
+import type { NextRequest } from "next/server";
 
 export async function middleware(req: NextRequest) {
-  const res = NextResponse.next()
-  const supabase = createMiddlewareSupabaseClient<Database>({ req, res })
-  await supabase.auth.getSession()
-  return res
+  const res = NextResponse.next();
+  const supabase = createMiddlewareSupabaseClient<Database>({ req, res });
+  const session = await supabase.auth.getSession();
+
+  if (!session) {
+    console.log('oh no no session exists!')
+    const redirectUrl = req.nextUrl.clone();
+    redirectUrl.pathname = "/login";
+    redirectUrl.searchParams.set(`redirectedFrom`, req.nextUrl.pathname);
+    return NextResponse.redirect(redirectUrl);
+  }
+  return res;
 }
