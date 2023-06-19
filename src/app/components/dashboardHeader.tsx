120375d1040d086cb8cfdd6e9e3a7ea3194a2097
Dharmin <dharmin@seatzapp.com>
2023-06-19 18:28:41 -0500

    updated to use new utility function

--- src/app/components/dashboardHeader.tsx
+++ src/app/components/dashboardHeader.tsx
@@ -20,56 +20,24 @@ import { PostgrestError, User } from "@supabase/supabase-js";
 import AddStore from "./utils/addStore";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { ProfileType } from "@/types";
+import { getSelectId } from "../utils/client/profile";
 
 export default function DashboardHeader() {
   const { supabase, session } = useSupabase();
   const [selectId, setSelectId] = useState<string | null>(null);
   const [user, SetUser] = useState<User | null | undefined>(session?.user);
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-  const [profile, SetProfile] = useState<ProfileType | null>(null);
-  const open = Boolean(anchorEl);
-  const [openDialog, setOpenDialog] = useState<boolean>(false);
 
-  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-  const handleDialogClose = () => {
-    setOpenDialog(false);
-  };
+  //import utils function
 
-  //maybe get all the profile data an memoiz just the select_id
-  //test if this changes from group
-  async function getProfileData() {
-    const { data, error } = await supabase
-      .from("profiles")
-      .select("*")
-      .eq("id", user?.id)
-      .single();
-    if (error) {
-      throw new Error(error.message);
-    } else {
-      SetProfile(data);
-    }
-  }
-  async function getSelectId() {
-    const { data, error } = await supabase
-      .from("profiles")
-      .select("select_id")
-      .eq("id", user?.id)
-      .single();
-    if (error) {
-      throw new Error(error.message);
-    } else {
-      setSelectId(data?.select_id);
+  async function getData() {
+    if (user?.id) {
+      const data = await getSelectId(supabase, user?.id);
+      data && setSelectId(data);
     }
   }
   useEffect(() => {
-    if (session?.user) {
-      getSelectId();
-      getProfileData();
+    if (session?.user && session?.user.id) {
+      getData();
     }
   }, [supabase]);
 
