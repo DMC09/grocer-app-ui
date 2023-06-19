54a5f27f8129ba8985e91e34b9f4b7a3f63aea9d
Dharmin <dharmin@seatzapp.com>
2023-06-19 17:53:11 -0500

    added client util functions for profile actions

---
+++ src/app/utils/client/profile.ts
@@ -0,0 +1,31 @@
+import { Database } from "@/types";
+import { SupabaseClient } from "@supabase/supabase-js";
+
+export async function getProfileData(supabase: SupabaseClient<Database>,userId:string) {
+    const { data, error } = await supabase
+      .from("profiles")
+      .select("*")
+      .eq("id", userId)
+      .single();
+    if (error) {
+      throw new Error(error.message);
+    } else {
+      return data
+    }
+  }
+
+  export async function handleChangeView(
+    supabase: SupabaseClient<Database>,
+    expanded: boolean,
+    profileId: string
+  ) {
+    const { data, error } = await supabase
+      .from("profiles")
+      .update({ expanded_groceryitem: !expanded })
+      .eq("id", profileId)
+      .single();
+  
+    if (error) {
+      throw new Error(error.message);
+    }
+  }
