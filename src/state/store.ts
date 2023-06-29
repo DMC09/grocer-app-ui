fae7f90f9a7e067f190fbc131997a90fa0d94efc
Dharmin <dharmin@seatzapp.com>
2023-06-29 00:16:23 -0500

    CRUD for grocerystore and grocerystoreitems

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,9 +1,16 @@
-import { GroceryStoreWithItemsType, ProfileType } from "@/types";
+import {
+  GroceryStoreItemType,
+  GroceryStoreType,
+  GroceryStoreWithItemsType,
+  ProfileType,
+} from "@/types";
 import { create } from "zustand";
 import { immer } from "zustand/middleware/immer";
 import { mountStoreDevtool } from "simple-zustand-devtools";
 import { devtools, persist } from "zustand/middleware";
+import { produce } from "immer";
 
+// Profiles
 interface ProfileState {
   data: ProfileType;
 }
@@ -46,33 +53,46 @@ const profileStore = immer<ProfileState & ProfileActions>((set, get) => ({
     }),
 }));
 
-export const useProfileStore = create(
-  devtools(persist(profileStore, { name: "Profile store" }))
-);
-
-if (process.env.NODE_ENV === "development") {
-  mountStoreDevtool("Profile store", useProfileStore);
-}
-
+// grocery store
 interface GroceryStoreState {
-  data: GroceryStoreWithItemsType;
+  data: GroceryStoreWithItemsType[];
 }
 
 type GroceryStoreActions = {
   resetStore: () => void;
-  setGroceryState: (fetchedData: GroceryStoreWithItemsType) => void;
+  setGroceryState: (fetchedData: GroceryStoreWithItemsType[]) => void;
+  addNewGroceryStore: (newStore: GroceryStoreWithItemsType) => void;
+  deleteGroceryStore: (storeId: number) => void;
+  updateGroceryStore: (updatedStoreData: GroceryStoreWithItemsType) => void;
+  insertGroceryItem: (item: GroceryStoreItemType) => void;
+  deleteGroceryItem: (itemId: number) => void;
+  updateGroceryItem: (updatedItemData: GroceryStoreItemType) => void;
 };
 
 const initialGroceryStoreState: GroceryStoreState = {
-  data: {
-    created_at: "",
-    id: 0,
-    image: "",
-    name: "",
-    quantity: null,
-    select_id: null,
-    grocerystoreitems: [],
-  },
+  data: [
+    {
+      created_at: "",
+      id: 0,
+      image: "",
+      name: "",
+      quantity: null,
+      select_id: null,
+      grocerystoreitems: [
+        {
+          created_at: null,
+          id: 0,
+          image: null,
+          modified_at: null,
+          name: null,
+          notes: null,
+          quantity: null,
+          select_id: null,
+          store_id: 0,
+        },
+      ],
+    },
+  ],
 };
 
 const GroceryStoreStore = immer<GroceryStoreState & GroceryStoreActions>(
@@ -81,10 +101,151 @@ const GroceryStoreStore = immer<GroceryStoreState & GroceryStoreActions>(
     resetStore: () => {
       set(initialGroceryStoreState);
     },
-    setGroceryState: (fetchedData: GroceryStoreWithItemsType) => {
+    setGroceryState: (fetchedData: GroceryStoreWithItemsType[]) => {
       set((state) => {
         state.data = fetchedData;
       });
     },
+    addNewGroceryStore: (newStore: GroceryStoreWithItemsType) => {
+      set(
+        produce((draft) => {
+          if (newStore) {
+            console.log("Adding store");
+            draft.data.push({ ...newStore, grocerystoreitems: [] });
+          }
+        }, initialGroceryStoreState)
+      );
+    },
+    deleteGroceryStore: (storeId: number) => {
+      set(
+        produce((draft) => {
+          if (storeId) {
+            console.log("deleting store");
+            console.log(draft.data);
+            const storeIndex = draft.data.findIndex(
+              (item: { id: number }) => item.id === storeId
+            );
+            draft.data.splice(storeIndex, 1);
+          }
+        }, initialGroceryStoreState)
+      );
+    },
+    updateGroceryStore: (updatedStoreData: GroceryStoreWithItemsType) => {
+      set(
+        produce((draft) => {
+          const storeIndex = draft.data.findIndex(
+            (s) => s.id === updatedStoreData?.id
+          );
+
+          const updatedStore = {
+            ...updatedStoreData,
+            grocerystoreitems: draft.data[storeIndex].grocerystoreitems,
+          };
+
+          draft.data[storeIndex] = updatedStore;
+        }, initialGroceryStoreState)
+      );
+    },
+    insertGroceryItem: (item: GroceryStoreItemType) => {
+      set(
+        produce((draft) => {
+          const storeIndex = draft.data.findIndex(
+            (s) => s.id === item.store_id
+          );
+
+          console.log(`adding new item to ${draft.data[storeIndex].name}`);
+
+          draft.data[storeIndex].grocerystoreitems.push(item);
+        }, initialGroceryStoreState)
+      );
+    },
+    deleteGroceryItem: (itemId: number) => {
+      set(
+        produce((draft) => {
+          const storeIndex = findGrocerystoreIndex(draft, itemId);
+          const itemIndex = findGrocerystoreitemIndexInStore(
+            draft,
+            itemId,
+            storeIndex
+          );
+
+          draft.data[storeIndex].grocerystoreitems.splice(itemIndex, 1);
+          // need the store id too.
+          // const storeIndex = draft.data.findIndex(
+          //   (s) => s.id === item.store_id
+          // );
+
+          // console.log(`adding new item to ${draft.data[storeIndex].name}`);
+
+          // draft.data[storeIndex].grocerystoreitems.push(item);
+        }, initialGroceryStoreState)
+      );
+    },
+    updateGroceryItem: (updatedItemData: GroceryStoreItemType) => {
+      set(
+        produce((draft) => {
+          const storeIndex = findGrocerystoreIndex(draft, updatedItemData.id);
+
+          const itemIndex = findGrocerystoreitemIndexInStore(
+            draft,
+            updatedItemData.id,
+            storeIndex
+          );
+
+          draft.data[storeIndex].grocerystoreitems[itemIndex] = updatedItemData;
+          // need the store id too.
+          // const storeIndex = draft.data.findIndex(
+          //   (s) => s.id === item.store_id
+          // );
+
+          // console.log(`adding new item to ${draft.data[storeIndex].name}`);
+
+          // draft.data[storeIndex].grocerystoreitems.push(item);
+        }, initialGroceryStoreState)
+      );
+    },
   })
 );
+
+export const useGroceryStoreStore = create(
+  devtools(persist(GroceryStoreStore, { name: "Grocery Store store" }))
+);
+export const useProfileStore = create(
+  devtools(persist(profileStore, { name: "Profile store" }))
+);
+
+if (process.env.NODE_ENV === "development") {
+  mountStoreDevtool("Grocery Store store", useGroceryStoreStore);
+}
+
+if (process.env.NODE_ENV === "development") {
+  mountStoreDevtool("Profile store", useProfileStore);
+}
+
+export function findGrocerystoreIndex(
+  state: GroceryStoreState,
+  grocerystoreitemId: number
+) {
+  const grocerystoreIndex = state.data.findIndex((grocerystore) => {
+    return grocerystore.grocerystoreitems.some((grocerystoreitem) => {
+      return grocerystoreitem.id === grocerystoreitemId;
+    });
+  });
+
+  return grocerystoreIndex === -1 ? -1 : 0;
+}
+
+export function findGrocerystoreitemIndexInStore(
+  state: GroceryStoreState,
+  grocerystoreitemId: number,
+  storeIndex: number
+) {
+  const grocerystore = state.data[storeIndex];
+  const grocerystoreitemIndex = grocerystore.grocerystoreitems.findIndex(
+    (grocerystoreitem) => {
+      return grocerystoreitem.id === grocerystoreitemId;
+    }
+  );
+
+  return grocerystoreitemIndex === -1 ? -1 : grocerystoreitemIndex;
+}
