9eb26ec45be6125a769902cdaa64cedab2527805
Dharmin <dharmin@seatzapp.com>
2023-07-03 15:01:16 -0500

    refactored to use zustand state

--- src/app/components/groceryStore/groceryStoreHeader.tsx
+++ src/app/components/groceryStore/groceryStoreHeader.tsx
@@ -1,69 +1,54 @@
 "use client";
 
 import { Box, Button, Card, IconButton, Typography } from "@mui/material";
-import { useRouter } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
-import { GroceryStoreType } from "@/types";
 import GroceryStoreHeaderMenu from "./groceryStoreHeaderMenu";
 import { theme } from "@/app/utils/theme";
 import { ThemeProvider } from "@emotion/react";
-import { useEffect, useState } from "react";
-import { useSupabase } from "../supabase/supabase-provider";
+import useStore from "@/app/hooks/useStore";
+import { useGroceryStoreStore } from "@/state/GrocerStore";
 
-export default function GroceryStoreHeader(groceryStore: GroceryStoreType) {
-  const [groceryStoreToRender, setGroceryStoreToRender] =
-    useState(groceryStore);
-  const { supabase } = useSupabase();
+export default function GroceryStoreHeader() {
+  const { store_id } = useParams();
 
-  useEffect(() => {
-    const channel = supabase
-      .channel("custom-grocerystore-channel")
-      .on(
-        "postgres_changes",
-        {
-          event: "UPDATE",
-          schema: "public",
-          table: "grocerystores",
-          filter: `id=eq.${groceryStore.id}`,
-        },
-        (payload) => setGroceryStoreToRender(payload.new as GroceryStoreType)
-      )
+  const groceryStoreData = useStore(
+    useGroceryStoreStore,
+    (state) => state?.data
+  )?.filter(
+    (grocerystore) => Number(grocerystore.id) === Number(store_id)
+  )?.[0];
 
-      .subscribe();
-
-    return () => {
-      supabase.removeChannel(channel);
-    };
-  }, [supabase]);
-  // import theme for this.
   const router = useRouter();
   return (
     <>
       <ThemeProvider theme={theme}>
-        <Card
-          sx={{
-            border: 1,
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-            backgroundColor: "primary.main",
-          }}
-        >
-          <IconButton
-            onClick={() => router.back()}
-            aria-label="add to grocery store"
-            color="secondary"
-            size="large"
+        {groceryStoreData && (
+          <Card
+            sx={{
+              border: 1,
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              backgroundColor: "primary.main",
+            }}
           >
-            <ArrowBackIosNewIcon sx={{ fontSize: 30 }} />
-          </IconButton>
-          <Typography color="secondary.main" variant="h3">
-            {groceryStoreToRender.name}
-          </Typography>
-          <Box sx={{ display: "flex" }}>
-            <GroceryStoreHeaderMenu {...groceryStore} />
-          </Box>
-        </Card>
+            <IconButton
+              onClick={() => router.back()}
+              aria-label="add to grocery store"
+              color="secondary"
+              size="large"
+            >
+              <ArrowBackIosNewIcon sx={{ fontSize: 30 }} />
+            </IconButton>
+            <Typography color="secondary.main" variant="h3">
+              {groceryStoreData?.name}
+            </Typography>
+            <Box sx={{ display: "flex" }}>
+              <GroceryStoreHeaderMenu {...groceryStoreData} />
+            </Box>
+          </Card>
+        )}
       </ThemeProvider>
     </>
   );
