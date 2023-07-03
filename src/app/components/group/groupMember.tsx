7007f2558f35ca366012571938c8fffb449cac67
Dharmin <dharmin@seatzapp.com>
2023-07-03 17:11:03 -0500

    updated group member component

--- src/app/components/group/groupMember.tsx
+++ src/app/components/group/groupMember.tsx
@@ -1,17 +1,19 @@
 "use client";
 
 import { GroupMemberType } from "@/types";
-import { Avatar, Typography } from "@mui/material";
+import { Avatar, Box, Typography } from "@mui/material";
 
 export default function GroupMember(member: GroupMemberType) {
   return (
     <>
-      <Avatar
-        sx={{ width: 32, height: 32 }}
-        src={`${process.env.NEXT_PUBLIC_SUPABASE_PROFILE}/${member?.avatar_url}`}
-      />
-      <p>{member.first_name}</p>
-      <p>{member.last_name}</p>
+      <Box sx={{width:100,display:"flex",flexFlow:"column",alignItems:"center",justifyContent:"center"}}>
+        <Avatar
+          sx={{ width: 32, height: 32 }}
+          src={`${process.env.NEXT_PUBLIC_SUPABASE_PROFILE}/${member?.avatar_url}`}
+        />
+        <Typography>{member.first_name}</Typography>
+        <Typography>{member.last_name}</Typography>
+      </Box>
     </>
   );
 }
