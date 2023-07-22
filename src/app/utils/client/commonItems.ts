8472c76901477d57499b68c38774d3858a049edd
Dharmin <dharmin@seatzapp.com>
2023-07-21 23:37:39 -0500

    added simple util for common items

---
+++ src/app/utils/client/commonItems.ts
@@ -0,0 +1,54 @@
+import { useCommonItemsStore } from "@/state/CommonItems";
+import { CommonGroceryStoreItemType, Database } from "@/types";
+import { SupabaseClient } from "@supabase/supabase-js";
+
+export async function addFromCommonItems(supabase: SupabaseClient<Database>) {
+  // When I do a check on the individal commonGroceryStoreItem
+  // store id needs to be the same
+  // could make a state for the common items
+  // Then part of that state can be the items to add?
+  // Quantity will differ,
+  // name,notes,select_id, and image should come from the object
+  //but we want image path to be changeable.
+
+  const commonItemsToAdd = null;
+
+  const { data, error } = await supabase
+    .from("grocerystoreitems")
+    .insert([
+      {
+        store_id: storeId,
+        name,
+        notes,
+        quantity: Number(quantity),
+        select_id: selectId,
+        image: imagePath,
+      },
+    ])
+    .select();
+}
+
+export async function addToCommonItems(supabase: SupabaseClient<Database>,) {
+
+    const {data, error} = await supabase.from("commonitems").insert({
+        item_name:item_name,
+        item_notes:item_notes,
+        select_id: selectId,
+        image: imagePath,
+        category:category
+
+
+    })
+}
+
+export async function getAllCommonItems(supabase: SupabaseClient<Database>) {
+  const { data, error } = await supabase.from("commonitems").select("*");
+
+  if (error) {
+    throw new Error(error.message);
+  } else {
+    useCommonItemsStore.setState({
+      catalog: data as CommonGroceryStoreItemType[],
+    });
+  }
+}
