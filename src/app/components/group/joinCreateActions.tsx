a9ec9c4efeb8e50f5c99619eab78a6fa128b6b9a
Dharmin <dharmin@seatzapp.com>
2023-07-24 22:31:11 -0500

    added styling

--- src/app/components/group/joinCreateActions.tsx
+++ src/app/components/group/joinCreateActions.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Divider, Chip } from "@mui/material";
+import { Divider, Chip, Box } from "@mui/material";
 import { profile } from "console";
 import CreateGroup from "../utils/createGroup";
 import JoinGroup from "../utils/joinGroup";
@@ -9,11 +9,23 @@ import { ProfileType } from "@/types";
 export default function JoinCreateActions(profile: ProfileType) {
   return (
     <>
-      {<JoinGroup {...profile} />}
-      <Divider>
-        <Chip label="or" />
-      </Divider>
-      {<CreateGroup {...profile} />}
+      <Box
+        textAlign={"center"}
+        sx={{
+          p:2,
+          border:1,
+          borderRadius: 8,
+          backgroundColor: "background.paper",
+          boxShadow:5
+
+        }}
+      >
+        {<JoinGroup {...profile} />}
+        <Divider>
+          <Chip label="or" />
+        </Divider>
+        {<CreateGroup {...profile} />}
+      </Box>
     </>
   );
 }
