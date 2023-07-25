477e7927eb41a325d396ac0c6ed95eb4694e0cd9
Dharmin <dharmin@seatzapp.com>
2023-07-24 22:31:01 -0500

    updating spacing and styling

--- src/app/components/group/groupMember.tsx
+++ src/app/components/group/groupMember.tsx
@@ -6,7 +6,16 @@ import { Avatar, Box, Typography } from "@mui/material";
 export default function GroupMember(member: GroupMemberType) {
   return (
     <>
-      <Box sx={{width:100,display:"flex",flexFlow:"column",alignItems:"center",justifyContent:"center"}}>
+      <Box
+        sx={{
+          width: 100,
+          display: "flex",
+          flexFlow: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          p: 2,
+        }}
+      >
         <Avatar
           sx={{ width: 32, height: 32 }}
           src={`${process.env.NEXT_PUBLIC_SUPABASE_PROFILE}/${member?.avatar_url}`}
