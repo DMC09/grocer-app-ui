2b25500e8c73c7b0a5a46372bd697a8c21fe18fc
Dharmin <dharmin@seatzapp.com>
2023-08-01 19:20:34 -0500

    refactored after extracting dialog logic to new context

--- src/components/groceryStore/groceryStoreHeaderMenu.tsx
+++ src/components/groceryStore/groceryStoreHeaderMenu.tsx
@@ -1,27 +1,17 @@
 "use client";
 // Imports
 import {
-  Button,
-  Card,
-  CardMedia,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogTitle,
   Divider,
   Fade,
   IconButton,
   ListItemIcon,
   Menu,
   MenuItem,
-  TextField,
-  useMediaQuery,
 } from "@mui/material";
+import ListAltIcon from "@mui/icons-material/ListAlt";
 import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 import ControlPointIcon from "@mui/icons-material/ControlPoint";
-import GridViewIcon from "@mui/icons-material/GridView";
-import TocIcon from "@mui/icons-material/Toc";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { Settings } from "@mui/icons-material";
 import { useSupabase } from "../supabase/supabase-provider";
@@ -29,119 +19,41 @@ import MenuIcon from "@mui/icons-material/Menu";
 import { useRouter } from "next/navigation";
 import { GroceryStoreType, ProfileType } from "@/types";
 import AddPhotoAlternateIcon from "@mui/icons-material/AddPhotoAlternate";
-import { User } from "@supabase/supabase-js";
-import {
-  addNewGroceryStoreItem,
-  deleteGroceryStore,
-  updateGroceryStore,
-} from "@/utils/client/groceryStore";
-import {
-  generateGroceryStoreItemImagePath,
-  handleGroceryStoreImageUpload,
-} from "@/utils/client/image";
-import { theme } from "@/utils/theme";
-
+import { deleteGroceryStore } from "@/utils/client/groceryStore";
 import { handleChangeGroceryStoreItemView } from "@/utils/client/profile";
 import CloseIcon from "@mui/icons-material/Close";
 import useZustandStore from "@/hooks/useZustandStore";
 import { ProfileDataStore } from "@/stores/ProfileDataStore";
-
+import CommonItemsDialog from "./groceryStoreHeader/commonItemsDialog";
+import { useDialogContext } from "@/context/DialogContext";
+import AddNewItemDialog from "./groceryStoreHeader/addNewItemDialog";
+import EditGroceryStoreDialog from "./groceryStoreHeader/editGroceryStoreDialog";
 
 export default function GroceryStoreHeaderMenu(groceryStore: GroceryStoreType) {
   const profileData = useZustandStore(ProfileDataStore, (state) => state?.data);
-
-  // bring in the zustand stuff.
-  // Hooks
   const { supabase, session } = useSupabase();
   const router = useRouter();
   // State
-  const [user, SetUser] = useState<User | null | undefined>(session?.user);
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
-  const fullScreen = useMediaQuery(theme.breakpoints.down("sm"));
-  const [quantity, setQuantity] = useState("");
-  const [notes, setNotes] = useState("");
-  const [openSettingsDialog, setOpenSettingsDialog] = useState<boolean>(false);
-  const [openCreateDialog, setOpenCreateDialog] = useState<boolean>(false);
-  const [imagePath, setImagePath] = useState<string | null>(null);
-  const [updatedGroceryStoreName, setUpdatedGroceryStoreName] =
-    useState<string>(groceryStore.name);
-  const [newGroceryStoreName, setNewGroceryStoreName] = useState<string>();
-  const [updatedImage, setUpdatedImage] = useState({
-    preview: groceryStore?.image,
-    raw: "",
-  });
-  const [newImage, setNewImage] = useState({
-    preview: "",
-    raw: "",
-  });
 
-  // Need to seperate dialogs for this,including seperate the state
+  // TODO: Click way listener
+
+  const {
+    handleCommonItemsDialogOpen,
+    handleAddNewItemDialogOpen,
+    handleStoreSettingsDialogOpen,
+  } = useDialogContext();
+
   async function handleOpenMenu(event: React.MouseEvent<HTMLElement>) {
     setAnchorEl(event.currentTarget);
   }
   async function handleCloseMenu() {
     setAnchorEl(null);
   }
-  async function handleSettingsDialogClose() {
-    setOpenSettingsDialog(false);
-  }
-  async function handleCreateDialogClose() {
-    setOpenCreateDialog(false);
-  }
 
-  async function handleDelete() {
+  async function handleDeleteGroceryStore() {
     await deleteGroceryStore(supabase, groceryStore.id, router);
-  }
-
-  async function handleSetUpdatedImage(event: any) {
-    if (event.target.files.length && groceryStore?.select_id) {
-      const generatedImagePath = await generateGroceryStoreItemImagePath(
-        groceryStore?.select_id
-      );
-      setImagePath(generatedImagePath);
-      setUpdatedImage({
-        preview: URL.createObjectURL(event.target.files[0]),
-        raw: event.target.files[0],
-      });
-    }
-  }
-
-  async function handleSetNewImage(event: any) {
-    if (event.target.files.length && groceryStore?.select_id) {
-      const generatedImagePath = await generateGroceryStoreItemImagePath(
-        groceryStore?.select_id
-      );
-      setImagePath(generatedImagePath);
-      setNewImage({
-        preview: URL.createObjectURL(event.target.files[0]),
-        raw: event.target.files[0],
-      });
-    }
-  }
-
-  async function handleUpdateGroceryStoreSettings() {
-    if (updatedImage.raw && imagePath) {
-      await handleGroceryStoreImageUpload(
-        supabase,
-        imagePath,
-        updatedImage?.raw
-      );
-      await updateGroceryStore(
-        supabase,
-        groceryStore.id,
-        updatedGroceryStoreName,
-        imagePath
-      );
-      setOpenSettingsDialog(false);
-    } else {
-      await updateGroceryStore(
-        supabase,
-        groceryStore.id,
-        updatedGroceryStoreName
-      );
-      setOpenSettingsDialog(false);
-    }
   }
 
   async function handleChangeView() {
@@ -154,42 +66,13 @@ export default function GroceryStoreHeaderMenu(groceryStore: GroceryStoreType) {
     }
   }
 
-  async function handleSubmitNewItem() {
-    if (groceryStore.select_id && newGroceryStoreName) {
-      if (newImage.raw && imagePath) {
-        await handleGroceryStoreImageUpload(supabase, imagePath, newImage?.raw);
-        await addNewGroceryStoreItem(
-          supabase,
-          groceryStore.id,
-          newGroceryStoreName,
-          notes,
-          Number(quantity),
-          groceryStore.select_id,
-          imagePath
-        );
-      } else {
-        await addNewGroceryStoreItem(
-          supabase,
-          groceryStore.id,
-          newGroceryStoreName,
-          notes,
-          Number(quantity),
-          groceryStore.select_id
-        );
-      }
-      setOpenCreateDialog(false);
-      setNewGroceryStoreName("");
-      setNotes("");
-      setQuantity("");
-      setNewImage({ preview: "", raw: "" });
-    }
-  }
-
   return (
     <>
       <IconButton
         sx={{ color: "primary.main" }}
-        aria-label={!open? "Close grocery store menu":"Open grocery store menu"}
+        aria-label={
+          !open ? "Close grocery store menu" : "Open grocery store menu"
+        }
         id="long-button"
         aria-controls={open ? "long-menu" : undefined}
         aria-expanded={open ? "true" : undefined}
@@ -233,13 +116,13 @@ export default function GroceryStoreHeaderMenu(groceryStore: GroceryStoreType) {
         transformOrigin={{ horizontal: "right", vertical: "top" }}
         anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
       >
-        <MenuItem onClick={() => setOpenCreateDialog(true)}>
+        <MenuItem onClick={handleAddNewItemDialogOpen}>
           <ListItemIcon>
             <ControlPointIcon fontSize="small" />
           </ListItemIcon>
           Add Item
         </MenuItem>
-        <MenuItem onClick={() => setOpenSettingsDialog(true)}>
+        <MenuItem onClick={handleStoreSettingsDialogOpen}>
           <ListItemIcon>
             <Settings fontSize="small" />
           </ListItemIcon>
@@ -251,9 +134,15 @@ export default function GroceryStoreHeaderMenu(groceryStore: GroceryStoreType) {
           </ListItemIcon>
           Change View
         </MenuItem> */}
+
+        <MenuItem onClick={handleCommonItemsDialogOpen}>
+          <ListItemIcon>
+            <ListAltIcon fontSize="small" />
+          </ListItemIcon>
+          Add Common Items
+        </MenuItem>
         <Divider />
-        <MenuItem onClick={handleDelete}>
-          {/* need a modal to show the store settings which right now is the nmae */}
+        <MenuItem onClick={handleDeleteGroceryStore}>
           <ListItemIcon>
             <DeleteForeverIcon fontSize="small" />
           </ListItemIcon>
@@ -261,161 +150,13 @@ export default function GroceryStoreHeaderMenu(groceryStore: GroceryStoreType) {
         </MenuItem>
       </Menu>
       <>
-        {/* Create Dialog */}
-        <Dialog
-          fullScreen={fullScreen}
-          open={openCreateDialog}
-          onClose={handleCreateDialogClose}
-        >
-          <DialogTitle align="center">Add new item</DialogTitle>
-          <DialogContent>
-            <TextField
-              autoFocus
-              margin="dense"
-              id="Name"
-              label="Name"
-              type="email"
-              fullWidth
-              variant="standard"
-              onChange={(e) => setNewGroceryStoreName(e.target.value)}
-              value={newGroceryStoreName}
-            />
-          </DialogContent>
-          <DialogContent>
-            <TextField
-              autoFocus
-              margin="dense"
-              id="Notes"
-              label="Notes"
-              type="email"
-              fullWidth
-              variant="standard"
-              onChange={(e) => setNotes(e.target.value)}
-              value={notes}
-            />
-          </DialogContent>
-          <DialogContent
-            sx={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              flexFlow: "column",
-            }}
-          >
-            <TextField
-              fullWidth
-              type="number"
-              id="outlined-basic"
-              label="Quantity"
-              variant="outlined"
-              onChange={(e) => setQuantity(e.target.value)}
-              value={quantity}
-            />
-            <Card
-              sx={{
-                maxWidth: 150,
-                mt: 4,
-              }}
-            >
-              {newImage.preview ? (
-                <CardMedia
-                  component="img"
-                  height="150"
-                  image={newImage.preview}
-                  alt={`Image of `}
-                />
-              ) : (
-                <CardMedia
-                  component="img"
-                  height="150"
-                  image={
-                    "https://filetandvine.com/wp-content/uploads/2015/07/pix-uploaded-placeholder.jpg"
-                  }
-                  alt={`Image of `}
-                />
-              )}
-            </Card>
-
-            <Button
-              variant="contained"
-              component="label"
-              startIcon={<AddPhotoAlternateIcon />}
-            >
-              Upload File
-              <input type="file" onChange={handleSetNewImage} hidden />
-            </Button>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={handleCreateDialogClose}>Cancel</Button>
-            <Button onClick={handleSubmitNewItem}>Submit</Button>
-          </DialogActions>
-        </Dialog>
+        <AddNewItemDialog {...groceryStore} />
       </>
       <>
-        {/* Settings Dialog */}
-        <Dialog
-          fullScreen={fullScreen}
-          id="grocery-store-settings-dialog"
-          open={openSettingsDialog}
-          onClose={handleSettingsDialogClose}
-        >
-          <DialogTitle align="center">Grocery Store Settings</DialogTitle>
-          <DialogContent>
-            <TextField
-              autoFocus
-              margin="dense"
-              id="Name"
-              label="Name"
-              fullWidth
-              variant="standard"
-              onChange={(e) => setUpdatedGroceryStoreName(e.target.value)}
-              value={updatedGroceryStoreName}
-            />
-          </DialogContent>
-          <DialogContent
-            sx={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              flexFlow: "column",
-            }}
-          >
-            <Card
-              sx={{
-                maxWidth: 150,
-              }}
-            >
-              {updatedImage.raw ? (
-                <CardMedia
-                  component="img"
-                  height="150"
-                  image={updatedImage.preview || ""}
-                  alt={`Image of `}
-                />
-              ) : (
-                <CardMedia
-                  component="img"
-                  height="150"
-                  image={`${process?.env?.NEXT_PUBLIC_SUPABASE_GROCERYSTORE}/${updatedImage.preview}`} //maybe this should be normal image??
-                  alt={`Image of `}
-                />
-              )}{" "}
-            </Card>
-
-            <Button
-              variant="contained"
-              component="label"
-              startIcon={<AddPhotoAlternateIcon />}
-            >
-              Upload File
-              <input type="file" onChange={handleSetUpdatedImage} hidden />
-            </Button>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={handleSettingsDialogClose}>Cancel</Button>
-            <Button onClick={handleUpdateGroceryStoreSettings}>Save</Button>
-          </DialogActions>
-        </Dialog>
+        <EditGroceryStoreDialog {...groceryStore} />
+      </>
+      <>
+        <CommonItemsDialog selectId={groceryStore.select_id} storeId={groceryStore.id} />
       </>
     </>
   );
