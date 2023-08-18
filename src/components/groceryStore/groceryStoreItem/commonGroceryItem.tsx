1c66c4ed18a691dececbc979803ae4412fec2006
Dharmin <dharmin@seatzapp.com>
2023-08-17 20:01:21 -0500

    Common Item Component

--- src/components/groceryStore/groceryStoreItem/commonGroceryItem.tsx
+++ src/components/groceryStore/groceryStoreItem/commonGroceryItem.tsx
@@ -1,13 +1,29 @@
 import { CommonItemsDataStore } from "@/stores/CommonItemsDataStore";
 import { CommonItemToAdd, CommonItemType } from "@/types";
-import { Box, Card, Checkbox, TextField, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Checkbox,
+  IconButton,
+  TextField,
+  Typography,
+} from "@mui/material";
 import { ChangeEvent, useEffect, useMemo, useRef, useState } from "react";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import RemoveCircleIcon from "@mui/icons-material/RemoveCircle";
 
 export default function CommonGroceryStoreItem(item: CommonItemType) {
   const [selected, setSelected] = useState<boolean | null>(null);
   const [quantity, setQuantity] = useState<number>(1);
-  const [index, setNewIndex] = useState<number | null>(null);
+  const [uniqueId, setUniqueId] = useState<number | null>(null);
   const itemsToSubmit = CommonItemsDataStore((state) => state.itemsToSubmit);
+
+  // not working again1!!
 
   const addItemToSubmit = CommonItemsDataStore(
     (state) => state.addItemToSubmit
@@ -20,10 +36,13 @@ export default function CommonGroceryStoreItem(item: CommonItemType) {
     (state) => state.removeItemToSubmit
   );
 
+  useEffect(() => {
+    console.log(itemsToSubmit, "Items To Submit?");
+  }, [itemsToSubmit]);
+
   const memoizedItemToAdd = useMemo(() => {
-    // cid and stuff needs to be thought out for
     const itemToAdd = {
-      index: index,
+      uniqueItemId: uniqueId,
       id: item.id,
       name: item.item_name,
       notes: item.item_notes,
@@ -31,92 +50,177 @@ export default function CommonGroceryStoreItem(item: CommonItemType) {
       image: item.image,
     };
 
+    console.log(itemToAdd, "Memoized Value Calculated");
     return itemToAdd;
-  }, [selected, quantity]);
+  }, [selected, quantity, uniqueId]);
 
-  function generateIndex() {
-    return itemsToSubmit.length;
+  function generateUiD() {
+    console.log("generating index");
+    if (itemsToSubmit.length === 0) {
+      console.log("Uid is 1 ");
+      setUniqueId(1);
+      // return 0;
+    } else {
+      const highestIndex = itemsToSubmit.reduce(
+        (prev, curr) => Math.max(prev, curr.uniqueItemId),
+        0
+      );
+      console.log(
+        `The highest index:${highestIndex}, The new index will be ${
+          highestIndex + 1
+        }`
+      );
+      setUniqueId(highestIndex + 1);
+    }
   }
 
   async function handleChecked(
-    event: ChangeEvent<HTMLInputElement>,
-    checked: boolean
+    event?: ChangeEvent<HTMLInputElement>,
+    checked?: boolean
   ) {
-    if (checked) {
-      setSelected(checked);
-      setNewIndex(generateIndex());
+    if (!selected) {
+      setSelected(true);
+      console.log("This item is checked", item.item_name);
+      generateUiD(); // should only be if is true
     } else {
-      setSelected(checked);
-      setQuantity(1);
+      setSelected(false);
     }
   }
 
   useEffect(() => {
     if (selected) {
-      addItemToSubmit(memoizedItemToAdd as any);
+      console.log(memoizedItemToAdd, "Item before adding to state to submit?");
+      if (uniqueId !== null)
+        addItemToSubmit(memoizedItemToAdd as CommonItemToAdd);
     } else {
-      removeItemToSubmit(memoizedItemToAdd.index as number);
+      console.log(memoizedItemToAdd, "Object after selected is false");
+      setQuantity(1);
+      if (uniqueId !== null) removeItemToSubmit(uniqueId);
     }
-  }, [selected]);
+  }, [selected, uniqueId]);
 
-  // need to get styles object for selected andone for not styles
+  useEffect(() => {
+    console.log(memoizedItemToAdd.quantity, "quantity?");
+    if (memoizedItemToAdd.quantity && selected) {
+      console.log(quantity, "quantity");
+      updateItemToSubmit(
+        memoizedItemToAdd.uniqueItemId as number,
+        memoizedItemToAdd.quantity as number
+      );
+    }
+  }, [memoizedItemToAdd.quantity]);
 
   async function handleQuantityChange(
     event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
   ) {
-    console.log(event.target.value, "updated qty");
     if (event.target.value) {
       setQuantity(Number(event.target.value));
-      updateItemToSubmit(
-        memoizedItemToAdd.index as number,
-        Number(event.target.value)
-      );
     }
   }
 
+  async function increment() {
+    console.log("Incrementing");
+    setQuantity(Number(quantity + 1));
+  }
+  async function decrement() {
+    console.log("Decrementing");
+    setQuantity(Number(quantity - 1));
+  }
+
+  const normalStyle = {
+    border: 1,
+    borderColor: "primary.light",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderRadius: 3,
+    height: 50,
+    m: 1,
+  };
+
+  const selectedStyle = {
+    border: 2,
+    borderColor: "primary.main",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderRadius: 3,
+    m: 1,
+  };
+
   return (
-    <Box
-      sx={{
-        border: selected ? 3 : 1,
-        display: "flex",
-        m: 1,
-        height: "33%",
-      }}
-    >
-      <Checkbox size="small" onChange={handleChecked} checked={!!selected} />
-      <Card
-        sx={{
-          display: "flex",
-          p: 1,
-        }}
-      >
-        <Typography variant="h5">{item.item_name}</Typography>
+    <>
+      <Card raised={!!selected} sx={selected ? selectedStyle : normalStyle}>
+        <CardActionArea
+          onClick={() => {
+            handleChecked();
+          }}
+        >
+          <CardContent
+            sx={{
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <Checkbox
+              size="small"
+              onChange={handleChecked}
+              checked={!!selected}
+              sx={{}}
+            />
+            <Typography variant="h5">{item.item_name}</Typography>
+            <Typography variant="body2" color="text.secondary"></Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions sx={{}}>
+          <IconButton
+            onClick={decrement}
+            aria-label="delete"
+            disabled={(selected ? false : true) || quantity < 2}
+            color="primary"
+          >
+            <RemoveCircleIcon />
+          </IconButton>
+
+          <TextField
+            size={"small"}
+            InputProps={{
+              inputMode: "numeric",
+              sx: {
+                "& input": {
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  textAlign: "center",
+                  width: 30,
+                  p: 0,
+                  m: 0,
+                },
+              },
+            }}
+            sx={{}}
+            onChange={(e) => {
+              handleQuantityChange(e);
+            }}
+            disabled={selected ? false : true}
+            value={quantity}
+            type="tel"
+            InputLabelProps={{
+              shrink: true,
+            }}
+            inputProps={{}}
+          />
+
+          <IconButton
+            onClick={increment}
+            aria-label="delete"
+            disabled={selected ? false : true}
+            color="primary"
+          >
+            <AddCircleIcon />
+          </IconButton>
+        </CardActions>
       </Card>
-      {selected ? (
-        <TextField
-          value={quantity}
-          type="number"
-          onChange={(e) => {
-            handleQuantityChange(e);
-          }}
-          InputLabelProps={{
-            shrink: true,
-          }}
-          inputProps={{
-            inputMode: "numeric",
-            pattern: "[0-9]*",
-          }}
-        />
-      ) : (
-        <TextField
-          disabled
-          value={quantity}
-          type="number"
-          InputLabelProps={{
-            shrink: true,
-          }}
-        />
-      )}
-    </Box>
+    </>
   );
 }
