6b0fb03cdfe2d3aa50b30d4954599b742f735e62
Dharmin <dharmin@seatzapp.com>
2023-08-19 16:51:48 -0500

    CRUD Common Item Helpers

--- src/helpers/commonItem.ts
+++ src/helpers/commonItem.ts
@@ -71,7 +71,7 @@ export async function addToCommonItemCatalog(
 
 export async function getAllCommonItems(supabase: SupabaseClient<Database>) {
   const { data, error } = await supabase.from("commonitems").select("*");
-
+  console.log("fetching common itemS1");
   if (error) {
     throw new Error(error.message);
   } else {
@@ -94,5 +94,114 @@ export function isCommonItemDataStoreEmpty(
     return false;
   } else {
     return true;
+  }
+}
+
+export async function updateCommonItem(
+  supabase: SupabaseClient<Database>,
+  id: number,
+  name: string | null,
+  notes: string | null,
+  imagePath: string | null
+) {
+  if (imagePath) {
+    console.log(imagePath, "image path");
+    const { data, error } = await supabase
+      .from("commonitems")
+      .update({
+        item_name: name,
+        item_notes: notes,
+        image: imagePath,
+      })
+      .eq("id", id)
+      .select()
+      .single();
+
+    if (error) {
+      throw new Error(error?.message);
+    } else {
+      console.log(data, "Updated Common Item but with new image");
+      return data;
+    }
+  } else {
+    const { data, error } = await supabase
+      .from("commonitems")
+      .update({
+        item_name: name,
+        item_notes: notes,
+      })
+      .eq("id", id)
+      .select()
+      .single();
+
+    if (error) {
+      throw new Error(error?.message);
+    } else {
+      console.log(data, "Updated Common Item but with no new image");
+      return data;
+    }
+  }
+}
+
+export async function addCommonItem(
+  supabase: SupabaseClient<Database>,
+  name: string,
+  notes: string,
+  selectId: string,
+  image: string | null
+) {
+  if (image) {
+    const { data, error } = await supabase
+      .from("commonitems")
+      .insert({
+        item_name: name,
+        item_notes: notes,
+        select_id: selectId,
+        image,
+      })
+      .select()
+      .single();
+    if (error) {
+      throw new Error(error?.message);
+    } else {
+      console.log(data, "Inserted new common item with picture");
+      return data;
+    }
+  } else {
+    const { data, error } = await supabase
+      .from("commonitems")
+      .insert({
+        item_name: name,
+        item_notes: notes,
+        select_id: selectId,
+      })
+      .select()
+      .single();
+
+    if (error) {
+      throw new Error(error.message);
+    } else {
+      console.log(data, "Inserted new common item with no  picture");
+      return data;
+    }
+  }
+}
+
+export async function deleteCommonItem(
+  supabase: SupabaseClient<Database>,
+  id: number
+) {
+  const { data, error } = await supabase
+    .from("commonitems")
+    .delete()
+    .eq("id", id)
+    .select()
+    .single();
+
+  if (error) {
+    throw new Error(error.message);
+  } else {
+    console.log(data, "data after deleting a common id ");
+    return data;
   }
 }
