2f1c2b7920d2999233a719fe515085b8a4b015b2
Dharmin <dharmin@seatzapp.com>
2023-08-27 18:49:07 -0500

    moved steps for .env

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,7 +4,7 @@ import { defineConfig, devices } from "@playwright/test";
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-require("dotenv").config({ path: "./.env.local" });
+require("dotenv").config({ path: "./.env" });
 
 
 /**
