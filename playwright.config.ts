841a2adc57f677428f41b68baa7ee2643a62d59c
Dharmin <dharmin@seatzapp.com>
2023-08-27 19:54:37 -0500

    updating report and added github pages deployment

--- playwright.config.ts
+++ playwright.config.ts
@@ -39,9 +39,9 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
 
-    trace: "retain-on-failure",
-    screenshot: "only-on-failure",
-    video: "retain-on-failure",
+    trace: "on",
+    screenshot: "on",
+    video: "on",
   },
 
   /* Configure projects for major browsers */
