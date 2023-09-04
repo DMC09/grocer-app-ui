83bc108334618bd8bbd157dfc0d851a653d03afd
Dharmin <dharmin@seatzapp.com>
2023-09-04 11:59:43 -0500

    added logo to layout in login page

--- src/app/login/layout.tsx
+++ src/app/login/layout.tsx
@@ -1,8 +1,14 @@
 "use client";
 
 import { theme } from "@/helpers/theme";
-import { Container, ThemeProvider, Typography } from "@mui/material";
-
+import {
+  Card,
+  CardMedia,
+  Container,
+  ThemeProvider,
+  Typography,
+} from "@mui/material";
+import logo from "../../../Logo.png";
 
 export default function LoginLayout({
   children,
@@ -23,9 +29,27 @@ export default function LoginLayout({
             backgroundColor: "primary.light",
           }}
         >
-          <Typography align="center" color="primary.dark" variant="h4">
-            Welcome! Login or Sign up to Continue
-          </Typography>
+         
+          <Card
+
+              sx={{
+                background: "none",
+                height: 100,
+                width: 100,
+                boxShadow:0
+              }}
+            >
+              <CardMedia
+                sx={{ objectFit: "fill" }}
+                component="img"
+                image={"../../../Logo.png"}
+                alt={`Preview  `}
+              />
+            </Card>
+            <Typography align="center" color="primary.dark" variant="h4">
+          
+Login or Sign up to Continue
+        </Typography>
           {children}
         </Container>
       </ThemeProvider>
