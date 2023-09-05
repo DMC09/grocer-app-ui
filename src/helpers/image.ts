12b2781a420e485b2fdc7325606f0f252c280132
Dharmin <dharmin@seatzapp.com>
2023-09-05 18:52:57 -0500

    new method to generate image path using new enum

--- src/helpers/image.ts
+++ src/helpers/image.ts
@@ -1,5 +1,34 @@
-import { Database } from "@/types";
+import {  Database, ImageType } from "@/types";
 import { SupabaseClient } from "@supabase/supabase-js";
+
+export async function generateImagePath(select_id: string, type: ImageType) {
+  const partialId = select_id?.slice(-16);
+  const timeStamp = new Date().getTime();
+
+  if (Object.values(ImageType).includes(type)) {
+    return `${type}/${partialId}/${timeStamp}`;
+  } else {
+    throw new Error(`Unknown image type ${type}`);
+  }
+}
+
+export async function handleImageUpload(
+  supabase: SupabaseClient<Database>,
+  imagePath: string,
+  imageRaw: string,
+  bucketType: BucketType
+) {
+  const { data, error } = await supabase?.storage
+    .from(bucketType)
+    .upload(imagePath, imageRaw);
+  if (error) {
+    throw new Error(
+      `Error uploading image to the ${bucketType} bucket ${error.message}`
+    );
+  } else {
+    console.log(data, "image uploaded successfully");
+  }
+}
 
 export async function generateStoreImagePath(select_id: string) {
   //Formula is last 16 characters of select_id + Current DateTime in seconds/
