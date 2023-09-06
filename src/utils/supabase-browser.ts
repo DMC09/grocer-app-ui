e082a8e257d45e6bed64a3a2ce2346d63c1cf3fc
Dharmin <dharmin@seatzapp.com>
2023-09-05 22:45:27 -0500

    removed old files

--- src/utils/supabase-browser.ts
+++
@@ -1,7 +0,0 @@
-
-import { Database } from '@/types';
-import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
-
-
-export const createBrowserClient = () =>
-  createBrowserSupabaseClient<Database>();
