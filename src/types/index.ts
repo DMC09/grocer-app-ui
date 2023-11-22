b8f01feee10f8904d03e666c4b665483d5391b43
Dharmin <dharmin@seatzapp.com>
2023-11-21 21:57:12 -0600

    updated types for adding category to grocerystoreitems

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,267 +1,273 @@
 import { AppRouterInstance } from "next/dist/shared/lib/app-router-context";
 import { string } from "yup";
-
 export type Json =
   | string
   | number
   | boolean
   | null
   | { [key: string]: Json | undefined }
-  | Json[];
+  | Json[]
 
 export interface Database {
   public: {
     Tables: {
       commonitems: {
         Row: {
-          category: string | null;
-          id: number;
-          image: string | null;
-          item_name: string ;
-          item_notes: string | null;
-          select_id: string | null;
-        };
+          category: string | null
+          id: number
+          image: string | null
+          item_name: string | null
+          item_notes: string | null
+          select_id: string | null
+        }
         Insert: {
-          category?: string | null;
-          id?: number;
-          image?: string | null;
-          item_name?: string ;
-          item_notes?: string | null;
-          select_id?: string | null;
-        };
+          category?: string | null
+          id?: number
+          image?: string | null
+          item_name?: string | null
+          item_notes?: string | null
+          select_id?: string | null
+        }
         Update: {
-          category?: string | null;
-          id?: number;
-          image?: string | null;
-          item_name?: string ;
-          item_notes?: string | null;
-          select_id?: string | null;
-        };
-        Relationships: [];
-      };
+          category?: string | null
+          id?: number
+          image?: string | null
+          item_name?: string | null
+          item_notes?: string | null
+          select_id?: string | null
+        }
+        Relationships: []
+      }
       grocerystoreitems: {
         Row: {
-          cid: number | null;
-          created_at: string | null;
-          id: number;
-          image: string | null;
-          modified_at: string | null;
-          name: string;
-          notes: string | null;
-          quantity: number;
-          select_id: string | null;
-          store_id: number;
-        };
+          category: string | null
+          cid: number | null
+          created_at: string | null
+          id: number
+          image: string | null
+          modified_at: string | null
+          name: string | null
+          notes: string | null
+          quantity: number | null
+          select_id: string | null
+          store_id: number
+        }
         Insert: {
-          cid?: number | null;
-          created_at?: string | null;
-          id?: number;
-          image?: string | null;
-          modified_at?: string | null;
-          name?: string;
-          notes?: string | null;
-          quantity?: number;
-          select_id?: string | null;
-          store_id: number;
-        };
+          category?: string | null
+          cid?: number | null
+          created_at?: string | null
+          id?: number
+          image?: string | null
+          modified_at?: string | null
+          name?: string | null
+          notes?: string | null
+          quantity?: number | null
+          select_id?: string | null
+          store_id: number
+        }
         Update: {
-          cid?: number | null;
-          created_at?: string | null;
-          id?: number;
-          image?: string | null;
-          modified_at?: string | null;
-          name?: string;
-          notes?: string | null;
-          quantity?: number;
-          select_id?: string | null;
-          store_id?: number;
-        };
+          category?: string | null
+          cid?: number | null
+          created_at?: string | null
+          id?: number
+          image?: string | null
+          modified_at?: string | null
+          name?: string | null
+          notes?: string | null
+          quantity?: number | null
+          select_id?: string | null
+          store_id?: number
+        }
         Relationships: [
           {
-            foreignKeyName: "grocerystoreitems_cid_fkey";
-            columns: ["cid"];
-            referencedRelation: "commonitems";
-            referencedColumns: ["id"];
+            foreignKeyName: "grocerystoreitems_cid_fkey"
+            columns: ["cid"]
+            isOneToOne: false
+            referencedRelation: "commonitems"
+            referencedColumns: ["id"]
           },
           {
-            foreignKeyName: "grocerystoreitems_store_id_fkey";
-            columns: ["store_id"];
-            referencedRelation: "grocerystores";
-            referencedColumns: ["id"];
+            foreignKeyName: "grocerystoreitems_store_id_fkey"
+            columns: ["store_id"]
+            isOneToOne: false
+            referencedRelation: "grocerystores"
+            referencedColumns: ["id"]
           }
-        ];
-      };
+        ]
+      }
       grocerystores: {
         Row: {
-          created_at: string | null;
-          id: number;
-          image: string | null;
-          name: string;
-          quantity: number;
-          select_id: string ;
-        };
+          created_at: string | null
+          id: number
+          image: string | null
+          name: string
+          quantity: number | null
+          select_id: string | null
+        }
         Insert: {
-          created_at?: string | null;
-          id?: number;
-          image?: string | null;
-          name: string;
-          quantity?: number;
-          select_id?: string | null;
-        };
+          created_at?: string | null
+          id?: number
+          image?: string | null
+          name: string
+          quantity?: number | null
+          select_id?: string | null
+        }
         Update: {
-          created_at?: string | null;
-          id?: number;
-          image?: string | null;
-          name?: string;
-          quantity?: number;
-          select_id?: string ;
-        };
-        Relationships: [];
-      };
+          created_at?: string | null
+          id?: number
+          image?: string | null
+          name?: string
+          quantity?: number | null
+          select_id?: string | null
+        }
+        Relationships: []
+      }
       groups: {
         Row: {
-          confirmation_accepted: boolean | null;
-          created_at: string | null;
-          email: string | null;
-          first_name: string | null;
-          group_id: string | null;
-          group_image: string | null;
-          group_name: string | null;
-          id: number;
-          is_admin: boolean | null;
-          last_name: string | null;
-          modified_at: string | null;
-          profile_id: string | null;
-          profile_image: string | null;
-          share_code: string | null;
-        };
+          confirmation_accepted: boolean | null
+          created_at: string | null
+          email: string | null
+          first_name: string | null
+          group_id: string | null
+          group_image: string | null
+          group_name: string | null
+          id: number
+          is_admin: boolean | null
+          last_name: string | null
+          modified_at: string | null
+          profile_id: string | null
+          profile_image: string | null
+          share_code: string | null
+        }
         Insert: {
-          confirmation_accepted?: boolean | null;
-          created_at?: string | null;
-          email?: string | null;
-          first_name?: string | null;
-          group_id?: string | null;
-          group_image?: string | null;
-          group_name?: string | null;
-          id?: number;
-          is_admin?: boolean | null;
-          last_name?: string | null;
-          modified_at?: string | null;
-          profile_id?: string | null;
-          profile_image?: string | null;
-          share_code?: string | null;
-        };
+          confirmation_accepted?: boolean | null
+          created_at?: string | null
+          email?: string | null
+          first_name?: string | null
+          group_id?: string | null
+          group_image?: string | null
+          group_name?: string | null
+          id?: number
+          is_admin?: boolean | null
+          last_name?: string | null
+          modified_at?: string | null
+          profile_id?: string | null
+          profile_image?: string | null
+          share_code?: string | null
+        }
         Update: {
-          confirmation_accepted?: boolean | null;
-          created_at?: string | null;
-          email?: string | null;
-          first_name?: string | null;
-          group_id?: string | null;
-          group_image?: string | null;
-          group_name?: string | null;
-          id?: number;
-          is_admin?: boolean | null;
-          last_name?: string | null;
-          modified_at?: string | null;
-          profile_id?: string | null;
-          profile_image?: string | null;
-          share_code?: string | null;
-        };
+          confirmation_accepted?: boolean | null
+          created_at?: string | null
+          email?: string | null
+          first_name?: string | null
+          group_id?: string | null
+          group_image?: string | null
+          group_name?: string | null
+          id?: number
+          is_admin?: boolean | null
+          last_name?: string | null
+          modified_at?: string | null
+          profile_id?: string | null
+          profile_image?: string | null
+          share_code?: string | null
+        }
         Relationships: [
           {
-            foreignKeyName: "groups_profile_id_fkey";
-            columns: ["profile_id"];
-            referencedRelation: "profiles";
-            referencedColumns: ["id"];
+            foreignKeyName: "groups_profile_id_fkey"
+            columns: ["profile_id"]
+            isOneToOne: false
+            referencedRelation: "profiles"
+            referencedColumns: ["id"]
           }
-        ];
-      };
+        ]
+      }
       profiles: {
         Row: {
-          avatar_url: string | null;
-          created_at: string;
-          email: string;
-          expanded_dashboard: boolean;
-          expanded_groceryitem: boolean;
-          first_name: string ;
-          id: string;
-          in_group: boolean | null;
-          last_name: string ;
-          phone: string | null;
-          select_id: string | null;
-          updated_at: string;
-        };
+          avatar_url: string | null
+          created_at: string
+          email: string
+          expanded_dashboard: boolean
+          expanded_groceryitem: boolean
+          first_name: string | null
+          id: string
+          in_group: boolean | null
+          last_name: string | null
+          phone: string | null
+          select_id: string | null
+          updated_at: string
+        }
         Insert: {
-          avatar_url?: string | null;
-          created_at?: string;
-          email: string;
-          expanded_dashboard?: boolean;
-          expanded_groceryitem?: boolean;
-          first_name?: string ;
-          id: string;
-          in_group?: boolean | null;
-          last_name?: string ;
-          phone?: string | null;
-          select_id?: string | null;
-          updated_at?: string;
-        };
+          avatar_url?: string | null
+          created_at?: string
+          email: string
+          expanded_dashboard?: boolean
+          expanded_groceryitem?: boolean
+          first_name?: string | null
+          id: string
+          in_group?: boolean | null
+          last_name?: string | null
+          phone?: string | null
+          select_id?: string | null
+          updated_at?: string
+        }
         Update: {
-          avatar_url?: string | null;
-          created_at?: string;
-          email?: string;
-          expanded_dashboard?: boolean;
-          expanded_groceryitem?: boolean;
-          first_name?: string ;
-          id?: string;
-          in_group?: boolean | null;
-          last_name?: string ;
-          phone?: string | null;
-          select_id?: string | null;
-          updated_at?: string;
-        };
-        Relationships: [];
-      };
-    };
+          avatar_url?: string | null
+          created_at?: string
+          email?: string
+          expanded_dashboard?: boolean
+          expanded_groceryitem?: boolean
+          first_name?: string | null
+          id?: string
+          in_group?: boolean | null
+          last_name?: string | null
+          phone?: string | null
+          select_id?: string | null
+          updated_at?: string
+        }
+        Relationships: []
+      }
+    }
     Views: {
       group_members_view: {
         Row: {
-          avatar_url: string | null;
-          email: string | null;
-          first_name: string | null;
-          group_id: string | null;
-          group_image: string | null;
-          group_name: string | null;
-          id: number | null;
-          last_name: string | null;
-        };
-        Relationships: [];
-      };
-    };
+          avatar_url: string | null
+          email: string | null
+          first_name: string | null
+          group_id: string | null
+          group_image: string | null
+          group_name: string | null
+          id: number | null
+          last_name: string | null
+        }
+        Relationships: []
+      }
+    }
     Functions: {
       get_groups_for_authenticated_user: {
-        Args: Record<PropertyKey, never>;
-        Returns: string[];
-      };
+        Args: Record<PropertyKey, never>
+        Returns: string[]
+      }
       join_group_via_share_code: {
         Args: {
-          share_code: string;
-          email: string;
-          first_name: string;
-          last_name: string;
-          profile_id: string;
-          profile_image: string;
-        };
-        Returns: boolean;
-      };
-    };
+          share_code: string
+          email: string
+          first_name: string
+          last_name: string
+          profile_id: string
+          profile_image: string
+        }
+        Returns: boolean
+      }
+    }
     Enums: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     CompositeTypes: {
-      [_ in never]: never;
-    };
-  };
+      [_ in never]: never
+    }
+  }
 }
+
 
 export type GroceryStoreType =
   Database["public"]["Tables"]["grocerystores"]["Row"];
