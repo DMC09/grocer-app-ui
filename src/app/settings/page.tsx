8282fee35b3c354aae7be0f0fcf95541989109af
Dharmin <dharmin@seatzapp.com>
2024-02-26 21:27:31 -0600

    Updated setting tabs

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -9,7 +9,6 @@ import { ProfileDataStore } from "@/stores/ProfileDataStore";
 import CommonItemsSettings from "@/components/settings/commonItemsSettings";
 import CategoriesSettings from "@/components/settings/categoriesSettings";
 
-
 export default function Settings() {
   const profileData = useZustandStore(ProfileDataStore, (state) => state?.data);
   const [currentTabIndex, setCurrentTabIndex] = useState(0);
@@ -40,20 +39,35 @@ export default function Settings() {
           </Typography>
         </Box>
 
-        <Box sx={{}}>
-          <Tabs value={currentTabIndex} onChange={handleTabChange}>
-            <Tab label="Profile" />
-            <Tab label="Group" />
-            <Tab label="Common Items" />
-            <Tab label="Categories" />
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "flexStart",
+          }}
+        >
+          <Tabs
+            value={currentTabIndex}
+
+            variant="scrollable"
+            onChange={handleTabChange}
+            allowScrollButtonsMobile
+            sx={{
+              width: "90%",
+            }}
+          >
+            <Tab sx={{}} label="Profile" />
+            <Tab sx={{}} label="Group" />
+            <Tab sx={{}} label="Categories" />
+            <Tab sx={{}} label="Common Items" />
           </Tabs>
         </Box>
         {currentTabIndex === 0 && <ProfileSettings />}
         {currentTabIndex === 1 && profileData && (
           <GroupSettings {...profileData} />
-          )}
-          {currentTabIndex === 2 && <CommonItemsSettings />}
-          {currentTabIndex === 3 && <CategoriesSettings />}
+        )}
+        {currentTabIndex === 3 && <CommonItemsSettings />}
+        {currentTabIndex === 2 && <CategoriesSettings />}
       </Container>
     </>
   );
