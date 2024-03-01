e29a539593f989d8305d5a5c95d0413070ebae87
Dharmin <dharmin@seatzapp.com>
2024-03-01 13:36:16 -0600

    updated import for next app router instance

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,5 @@
-import { AppRouterInstance } from "next/dist/shared/lib/app-router-context"
+import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime"
+
 
 export type Json =
   | string
