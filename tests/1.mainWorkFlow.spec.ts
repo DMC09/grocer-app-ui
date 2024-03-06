d4fe2e2831d541a94d1fd61fd65c72185d8bd1e3
Dharmin <dharmin@seatzapp.com>
2024-03-06 12:00:10 -0600

    Rework in progress

--- tests/1.mainWorkFlow.spec.ts
+++ tests/1.mainWorkFlow.spec.ts
@@ -27,6 +27,85 @@ test.describe("Single User Scenarios", () => {
       await expect(page.getByText("No stores available...")).toBeVisible();
     });
 
+    await test.step("Go to Category Settings", async () => {
+      await page.getByLabel("Profile Menu").click();
+      await page.getByRole("menuitem", { name: "Settings" }).click();
+      await page.getByRole("tab", { name: "Categories" }).click();
+      await expect(page.getByText("No Categories available...")).toBeVisible();
+      await expect(page).toHaveURL("/settings", {
+        timeout: 120000,
+      });
+    });
+
+    await test.step("Create Meet Category", async () => {
+      await page.getByLabel("Add New Category").click();
+      await page.getByLabel("Category name").click();
+      await page.getByLabel("Category name").fill("Meet");
+      await page.getByRole("button", { name: "Submit" }).click();
+      await expect(
+        page.getByRole("button", { name: "Edit Meet category" })
+      ).toBeVisible();
+    });
+    await test.step("Create Dairy Category", async () => {
+      await page.getByLabel("Add New Category").click();
+      await page.getByLabel("Category name").click();
+      await page.getByLabel("Category name").fill("Dairy");
+      await page.getByRole("button", { name: "Submit" }).click();
+      await expect(
+        page.getByRole("button", { name: "Edit Dairy category" })
+      ).toBeVisible();
+    });
+    await test.step("Create Fruit Category", async () => {
+      await page.getByLabel("Add New Category").click();
+      await page.getByLabel("Category name").click();
+      await page.getByLabel("Category name").fill("Fruit");
+      await page.getByRole("button", { name: "Submit" }).click();
+      await expect(page.getByText("Fruit")).toBeVisible();
+      await expect(
+        page.getByRole("button", { name: "Edit Fruit category" })
+      ).toBeVisible();
+    });
+
+    await test.step("Go to common items settings ", async () => {
+      await page
+        .getByRole("tab", { name: "Common Items" })
+        .click({ delay: 2000 });
+    });
+
+    await test.step("Create Eggs Large Brown Common Item w/no category selected", async () => {
+      await page.getByLabel("Add New Common item").click({ delay: 2000 });
+      await page.getByLabel("Name").fill("Eggs");
+      await page.getByLabel("Notes").first().fill("Large Brown");
+      await page.getByRole("button", { name: "Submit" }).click();
+    });
+    await test.step("Create Banana Common Item w/ Fruit category", async () => {
+      await page.getByLabel("Add New Common item").click({ delay: 2000 });
+      await page.getByLabel("Name").fill("Banana");
+      await page.getByRole("combobox", { name: "--" }).click();
+      await page.getByRole("option", { name: "Fruit" }).click();
+      await page.getByRole("button", { name: "Submit" }).click();
+    });
+
+    await test.step("Create Chicken Thighs Skinless Common Item w/ Meet Category", async () => {
+      await page.getByLabel("Add New Common item").click();
+      await page.getByLabel("Name").fill("Chicken Thighs");
+      await page.getByLabel("Notes").fill("Skinless");
+      await page.getByRole("combobox", { name: "--" }).click();
+      await page.getByRole("option", { name: "Meet" }).click();
+      await page.getByRole("button", { name: "Submit" }).click();
+    });
+
+    await test.step("Go to Categories Settings ", async () => {
+      await page.getByRole("tab", { name: "Categories" }).click();
+    });
+    await test.step("Edit Meet Category to Meat", async () => {
+      await page.getByRole("button", { name: "Edit Meet category" }).click();
+      await page.getByRole("button", { name: "Submit" }).click();
+    });
+    await test.step("Go to home page", async () => {
+      await page.getByLabel("To Home").click();
+    });
+
     await test.step("Create a new store - Walmart", async () => {
       await page.getByLabel("Dashboard Menu").click({ delay: 500 });
       await page.getByRole("menuitem", { name: "Add new store" }).click();
@@ -66,19 +145,42 @@ test.describe("Single User Scenarios", () => {
       ).toBeVisible();
     });
 
-    await test.step("Update item notes to be sharp cheddar", async () => {
+    await test.step("Update item notes to be sharp cheddar and quantity to 2 and add category", async () => {
       await page.getByRole("button", { name: "Cheese Mild Cheddar" }).click();
       await page.getByLabel("Edit Item").click();
       await page.getByLabel("Notes").fill("Sharp Cheddar");
+      await page.getByRole("combobox", { name: "--" }).click();
+      await page.getByRole("option", { name: "Dairy" }).click();
       await page.getByLabel("Quantity").fill("02");
       await page.getByRole("button", { name: "Submit" }).click();
       await page.getByRole("heading", { name: "Sharp Cheddar" }).click();
       await page.keyboard.press("Escape");
     });
 
-    await test.step("Complete item (cheese)", async () => {
-      await page.getByLabel("Complete Cheese").click();
+    await test.step("Add Eggs and Chicken Thighs to Wally World ", async () => {
+      await page.getByLabel("Open grocery store menu").click();
+      await page.getByRole("menuitem", { name: "Add Common Item" }).click();
+      await page.getByRole("button", { name: "Eggs Large Brown" }).click();
+      await page
+        .getByRole("button", { name: "Chicken Thighs Skinless" })
+        .click();
+      await page.getByLabel("Increment Eggs").click();
+      await page.getByRole("button", { name: "Add" }).click();
     });
+
+    await test.step("Verify Cheese, Eggs and Thighs items ", async () => {
+      await page.getByRole("button", { name: "Diary" });
+    });
+
+    await test.step("Verify Dairy filter work ", async () => {
+      await page.getByRole("button", { name: "Diary" });
+      // Verify only Cheese Shows
+    });
+
+    await test.step("Add common item Eggs  ", async () => {});
+
+    await test.step("Verify Dairy filter wwork ", async () => {});
+
     await test.step("Add (Bread - Whole Wheat)", async () => {
       await page.getByLabel("Open grocery store menu").click();
       await page.getByRole("menuitem", { name: "Add Item" }).click();
@@ -96,125 +198,115 @@ test.describe("Single User Scenarios", () => {
       await page.keyboard.press("Escape");
     });
 
-    await test.step("Go to Common Item Catalog Settings ", async () => {
-      await page.getByLabel("Profile Menu").click({ delay: 2000 });
-      await page.getByRole("menuitem", { name: "Settings" }).click();
-      await expect(page).toHaveURL("/settings", {
-        timeout: 120000,
-      });
-      await page.getByRole("tab", { name: "Common Items" }).click();
-    });
+    // await test.step("Go to Common Item Catalog Settings ", async () => {
+    //   await page.getByLabel("Profile Menu").click({ delay: 2000 });
+    //   await page.getByRole("menuitem", { name: "Settings" }).click();
+    //   await expect(page).toHaveURL("/settings", {
+    //     timeout: 120000,
+    //   });
+    //   await page.getByRole("tab", { name: "Common Items" }).click();
+    // });
 
-    await test.step("Create new Common Item (Rice - Jasmine)", async () => {
-      await page.getByLabel("Add New Common item").click();
-      await page.getByLabel("Name").fill("Rice");
-      await page.getByLabel("Notes").fill("Jasmine");
-      await page.getByRole("button", { name: "Submit" }).click();
-    });
+    // await test.step("Create new Common Item (Rice - Jasmine)", async () => {
+    //   await page.getByLabel("Add New Common item").click();
+    //   await page.getByLabel("Name").fill("Rice");
+    //   await page.getByLabel("Notes").fill("Jasmine");
+    //   await page.getByRole("button", { name: "Submit" }).click();
+    // });
 
-    await test.step("Create new Common Item (Milk - Soy) ", async () => {
-      await page.getByLabel("Add New Common item").click();
-      await page.getByLabel("Name").fill("Milk");
-      await page.getByLabel("Notes").fill("Soy");
-      await page.getByRole("button", { name: "Submit" }).click();
-    });
-    await test.step("Add Milk and Rice to Wally World ", async () => {
-      await page.getByLabel("To Home").click();
-      await page.getByRole("button", { name: "Wally World" }).click();
-      await page.getByLabel("Open grocery store menu").click();
-      await page.getByRole("menuitem", { name: "Add Common Item" }).click();
-      await page.getByRole('button', { name: 'Rice Jasmine' }).click();
-      await page.getByRole('button', { name: 'Milk Soy' }).click();
-      await page.getByLabel('Increment Milk').click();    
-      await page.getByRole("button", { name: "Add" }).click();
-    });
+    // await test.step("Create new Common Item (Milk - Soy) ", async () => {
+    //   await page.getByLabel("Add New Common item").click();
+    //   await page.getByLabel("Name").fill("Milk");
+    //   await page.getByLabel("Notes").fill("Soy");
+    //   await page.getByRole("button", { name: "Submit" }).click();
+    // });
 
-    await test.step("Updating common item Rice -> (Quinoa-White)", async () => {
-      await page.getByLabel("Profile Menu").click();
-      await page.getByRole("menuitem", { name: "Settings" }).click();
-      await expect(page).toHaveURL("/settings", {
-        timeout: 120000,
-      });
-      await page.getByRole("tab", { name: "Common Items" }).click();
-      await page
-        .getByRole("button", { name: "Image of Rice Rice Jasmine" })
-        .click();
-      await page.getByLabel("Edit Common Item").click();
-      await page.getByLabel("Name").fill("Quinoa");
-      await page.getByLabel("Notes").fill("White");
-      await page.getByRole("button", { name: "Submit" }).click();
-      await page.getByLabel("Close Item Preview").click();
-    });
+    // await test.step("Updating common item Rice -> (Quinoa-White)", async () => {
+    //   await page.getByLabel("Profile Menu").click();
+    //   await page.getByRole("menuitem", { name: "Settings" }).click();
+    //   await expect(page).toHaveURL("/settings", {
+    //     timeout: 120000,
+    //   });
+    //   await page.getByRole("tab", { name: "Common Items" }).click();
+    //   await page
+    //     .getByRole("button", { name: "Image of Rice Rice Jasmine" })
+    //     .click();
+    //   await page.getByLabel("Edit Common Item").click();
+    //   await page.getByLabel("Name").fill("Quinoa");
+    //   await page.getByLabel("Notes").fill("White");
+    //   await page.getByRole("button", { name: "Submit" }).click();
+    //   await page.getByLabel("Close Item Preview").click();
+    // });
 
-    await test.step("Add (Quinoa - white) to Wally World", async () => {
-      await page.getByLabel("To Home").click();
-      await page.getByRole("button", { name: "Wally World" }).click();
-      await page.getByLabel("Open grocery store menu").click();
-      await page.getByRole("menuitem", { name: "Add Common Item" }).click();
-      await page.getByRole("button", { name: "Quinoa White" }).click();
-      await page.getByLabel("Increment Quinoa").click();
-      await page.getByRole("button", { name: "Add" }).click();
-    });
-    await test.step("Clear Items in the Wally World", async () => {
-      await page.getByLabel("Complete Bread").click();
-      await page.getByLabel("Complete Rice").click();
-      await page.getByLabel("Complete Milk").click();
-      await page.getByLabel("Complete Quinoa").click();
-    });
+    // await test.step("Add (Quinoa - white) to Wally World", async () => {
+    //   await page.getByLabel("To Home").click();
+    //   await page.getByRole("button", { name: "Wally World" }).click();
+    //   await page.getByLabel("Open grocery store menu").click();
+    //   await page.getByRole("menuitem", { name: "Add Common Item" }).click();
+    //   await page.getByRole("button", { name: "Quinoa White" }).click();
+    //   await page.getByLabel("Increment Quinoa").click();
+    //   await page.getByRole("button", { name: "Add" }).click();
+    // });
+    // await test.step("Clear Items in the Wally World", async () => {
+    //   await page.getByLabel("Complete Bread").click();
+    //   await page.getByLabel("Complete Rice").click();
+    //   await page.getByLabel("Complete Milk").click();
+    //   await page.getByLabel("Complete Quinoa").click();
+    // });
 
-    await test.step("Go to Common Item Catalog Settings ", async () => {
-      await page.getByLabel("Profile Menu").click({ delay: 2000 });
-      await page.getByRole("menuitem", { name: "Settings" }).click();
-      await expect(page).toHaveURL("/settings", {
-        timeout: 120000,
-      });
-      await page.getByRole("tab", { name: "Common Items" }).click();
-    });
+    // await test.step("Go to Common Item Catalog Settings ", async () => {
+    //   await page.getByLabel("Profile Menu").click({ delay: 2000 });
+    //   await page.getByRole("menuitem", { name: "Settings" }).click();
+    //   await expect(page).toHaveURL("/settings", {
+    //     timeout: 120000,
+    //   });
+    //   await page.getByRole("tab", { name: "Common Items" }).click();
+    // });
 
-    await test.step("Delete Bread from Common Items", async () => {
-      await page
-        .getByRole("button", { name: "Image of Bread Bread Whole Wheat" })
-        .click();
-      await page.getByLabel("Delete Common Item").click();
-    });
-    await test.step("Delete Milk from Common Items ", async () => {
-      await page
-        .getByRole("button", { name: "Image of Milk Milk Soy" })
-        .click();
-      await page.getByLabel("Delete Common Item").click();
-    });
-    await test.step("Delete Quinoa from Common Items ", async () => {
-      await page
-        .getByRole("button", { name: "Image of Quinoa Quinoa White" })
-        .click();
-      await page.getByLabel("Delete Common Item").click();
-    });
-    await test.step("Go to dashboard ", async () => {
-      await page.getByLabel("To Home").click();
-      await expect(page).toHaveURL("/dashboard", {
-        timeout: 120000,
-      });
-    });
+    // await test.step("Delete Bread from Common Items", async () => {
+    //   await page
+    //     .getByRole("button", { name: "Image of Bread Bread Whole Wheat" })
+    //     .click();
+    //   await page.getByLabel("Delete Common Item").click();
+    // });
+    // await test.step("Delete Milk from Common Items ", async () => {
+    //   await page
+    //     .getByRole("button", { name: "Image of Milk Milk Soy" })
+    //     .click();
+    //   await page.getByLabel("Delete Common Item").click();
+    // });
+    // await test.step("Delete Quinoa from Common Items ", async () => {
+    //   await page
+    //     .getByRole("button", { name: "Image of Quinoa Quinoa White" })
+    //     .click();
+    //   await page.getByLabel("Delete Common Item").click();
+    // });
+    // await test.step("Go to dashboard ", async () => {
+    //   await page.getByLabel("To Home").click();
+    //   await expect(page).toHaveURL("/dashboard", {
+    //     timeout: 120000,
+    //   });
+    // });
 
-    await test.step("Delete Wally World", async () => {
-      await page.getByRole("button", { name: "Wally World" }).click();
-      await expect(page).toHaveURL(new RegExp("/dashboard/grocerystores/*"), {
-        timeout: 1200000,
-      });
-      await page.getByLabel("Open grocery store menu").click();
-      await page.getByRole("menuitem", { name: "Delete Store" }).click();
-      await expect(page).toHaveURL("/dashboard", {
-        timeout: 120000,
-      });
-    });
+    // await test.step("Delete Wally World", async () => {
+    //   await page.getByRole("button", { name: "Wally World" }).click();
+    //   await expect(page).toHaveURL(new RegExp("/dashboard/grocerystores/*"), {
+    //     timeout: 1200000,
+    //   });
+    //   await page.getByLabel("Open grocery store menu").click();
+    //   await page.getByRole("menuitem", { name: "Delete Store" }).click();
+    //   await expect(page).toHaveURL("/dashboard", {
+    //     timeout: 120000,
+    //   });
+    // });
 
-    await test.step("Sign Out", async () => {
-      await page.getByLabel("Profile Menu").click();
-      await page.getByRole("menuitem", { name: "Sign Out" }).click();
-      await expect(page).toHaveURL("/login", {
-        timeout: 120000,
-      });
-      await page.close();
-    });
+    // await test.step("Sign Out", async () => {
+    //   await page.getByLabel("Profile Menu").click();
+    //   await page.getByRole("menuitem", { name: "Sign Out" }).click();
+    //   await expect(page).toHaveURL("/login", {
+    //     timeout: 120000,
+    //   });
+    //   await page.close();
+    // });
   });
 });
