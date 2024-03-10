cb5a0a0439862f8f2d66e3d2194d45bfd3069f4d
Dharmin <dharmin@seatzapp.com>
2024-03-10 16:42:09 -0500

    Updated empty state

--- src/components/utils/commonitems/noCommonItems.tsx
+++ src/components/utils/commonitems/noCommonItems.tsx
@@ -3,24 +3,21 @@ import { Box, Container, Typography } from "@mui/material";
 export default function NoCommonItems() {
   return (
     <>
-
-
-<Container
+      <Container
         sx={{
           display: "flex",
           flexFlow: "column",
           justifyContent: "center",
-          alignItems:"center",
+          alignItems: "center",
           textAlign: "center",
           height: "80%",
           p: 1,
         }}
       >
         <Typography variant="h4" color="text.secondary">
-        No common items found ...
+          No common items found ...
         </Typography>
       </Container>
-    
     </>
   );
 }
