8ab5f8455797e085652b0706293f9ae23b247aff
Dharmin <dharmin@seatzapp.com>
2024-03-16 13:22:28 -0500

    refactored to sort by store quantity

--- src/components/dashboard/allStoresView.tsx
+++ src/components/dashboard/allStoresView.tsx
@@ -2,13 +2,26 @@ import { GroceryStoreType } from "@/types";
 import GroceryStore from "../groceryStore/groceryStore";
 import { Container } from "@mui/material";
 import NoStores from "../utils/placeholders/noStores";
+import { useMemo } from "react";
 
-export default function allStoresView({
+export default function AllStoresView({
   groceryStores,
 }: {
   groceryStores: GroceryStoreType[] | [] | undefined;
 }) {
-  const groceryStoresToRender = groceryStores?.map(
+  const sortedGroceryStores = useMemo(() => {
+    if (!groceryStores) return []; // Handle empty array
+
+    const copy = [...groceryStores];
+    copy.sort((a, b) => {
+      const quantityA = a.quantity ?? 0;
+      const quantityB = b.quantity ?? 0;
+      return quantityB - quantityA; // Descending order
+    });
+    return copy;
+  }, [groceryStores]);
+
+  const groceryStoresToRender = sortedGroceryStores?.map(
     (groceryStore: GroceryStoreType) => {
       return <GroceryStore key={groceryStore.id} groceryStore={groceryStore} />;
     }
